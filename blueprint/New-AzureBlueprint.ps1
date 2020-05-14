a4bf8a6a9ce45beb0f4a3c53fd179d5029c3b46c
Jason Masten <jamasten@microsoft.com>
2020-05-14 16:47:05 -0400

    removed files

--- blueprint/New-AzureBlueprint.ps1
+++
@@ -1,4 +0,0 @@
-# Login first with Connect-AzAccount if not using Cloud Shell
-
-# Get a reference to the new blueprint object, we'll use it in subsequent steps
-$Blueprint = New-AzBlueprint -Name 'MyBlueprint' -BlueprintFile .\blueprint.json

91fabae8286aa856e7ec96cadabb58e393364424
Jason Masten <jamasten@microsoft.com>
2020-05-18 09:52:53 -0400

    organized files for subscription based ARM template for blueprint deployment

--- blueprint/New-AzureBlueprintDefinition.ps1
+++
@@ -1,7 +0,0 @@
-# Login first with Connect-AzAccount if not using Cloud Shell
-
-# Get a reference to the new blueprint object, we'll use it in subsequent steps
-$Blueprint = New-AzBlueprint -Name 'MyBlueprint' -BlueprintFile .\blueprint.json
-
-# Use the reference to the new blueprint object from the previous steps
-New-AzBlueprintArtifact -Blueprint $blueprint -Name 'roleContributor' -ArtifactFile .\artifacts\roleContributor.json

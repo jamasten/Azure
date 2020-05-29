84b5990542f1fe6d6658b5432a977b5fbc6b2e50
Jason Masten <jamasten@microsoft.com>
2020-05-28 22:55:58 -0400

    added parameters to PS subscription deployment cmdlet

--- Deploy-AzureSubscription.ps1
+++ Deploy-AzureSubscription.ps1
@@ -1,11 +1,14 @@
 $User = (Get-AzContext).Account.Id
 $TimeStamp = Get-Date -F 'yyyyMMddhhmmss'
 $Name =  $User.Split('@')[0] + '_' + $TimeStamp
+$UserObjectId = (Get-AzADUser -UserPrincipalName $User).Id
 
 New-AzSubscriptionDeployment `
   -Name $Name `
-  -Location eastus `
+  -Location 'eastus' `
   -TemplateFile '.\subscription.json' `
   -NamePrefixExternal 'jamasten' `
   -NamePrefixInternal 'ceastus' `
-  -UserObjectId $User
+  -UserObjectId $UserObjectId `
+  -SubnetId "[parameters('SubnetId')]" `
+  -LocationFromTemplate 'eastus'

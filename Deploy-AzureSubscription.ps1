3fdca09f7c9faad1d3e9df7fd87d6e4c29921853
Jason Masten <jamasten@microsoft.com>
2020-09-02 15:04:31 -0400

    testing all resources in json

--- Deploy-AzureSubscription.ps1
+++ Deploy-AzureSubscription.ps1
@@ -19,7 +19,7 @@ Param(
     [ValidateScript({(Get-AzLocation | Select-Object -ExpandProperty Location) -contains $_})]
     [string]$LocationSecondary, 
   
-    #Storage Account SKU
+    #Storage Account SKU: (p)remium or (s)tandard
     [Parameter(Mandatory=$true)]
     [ValidateSet("p", "s")]
     [string]$PerformanceType, 
@@ -55,21 +55,6 @@ if(!(Get-AzContext | Where-Object {$_.Subscription.Id -eq $SubscriptionId}))
 $User = (Get-AzADUser | Where-Object {$_.UserPrincipalName -like "$((Get-AzContext).Account.Id.Split('@')[0])*"}).Id
 $TimeStamp = Get-Date -F 'yyyyMMddhhmmss'
 $Name =  $User + '_' + $TimeStamp
-$ResourceGroups = @('identity','network','shared','wvd');
-$Subnets = @(
-    @{
-        "Name" = "shared";
-        "AddressPrefix" = "10.0.0.0/24";
-    },
-    @{
-        "Name" = "servers";
-        "AddressPrefix" = "10.0.1.0/24";
-    },
-    @{
-        "Name" = "wvd";
-        "AddressPrefix" = "10.0.2.0/24";
-    }
-)
 $VmUsername = Read-Host -Prompt 'Enter Virtual Machine Username' -AsSecureString
 $VmPassword = Read-Host -Prompt 'Enter virtual Machine Password' -AsSecureString
 $VSE = @{
@@ -77,72 +62,10 @@ $VSE = @{
     Environment = $Environment;
     Locations = @($LocationPrimary, $LocationSecondary);
     PerformanceType = $PerformanceType;
-    Subnets = $Subnets;
     User = $User
 }
 $VSE.Add("VmPassword", $VmPassword) # Secure Strings must use Add Method for proper deserialization
 $VSE.Add("VmUsername", $VmUsername) # Secure Strings must use Add Method for proper deserialization
-
-
-#############################################################
-# Create Resource Groups
-#############################################################
-# Resource Groups must be created in PS b/c the KEK can only be created in PS
-foreach ($Location in $VSE.Locations) 
-{
-    foreach ($Group in $ResourceGroups) 
-    {
-        try 
-        {
-            New-AzResourceGroup `
-                -Name $('rg-' + $Group + '-' + $VSE.Environment + '-' + $Location) `
-                -Location $Location `
-                -Force `
-                -ErrorAction Stop | Out-Null    
-        }
-        catch 
-        {
-            $_ | Select-Object *
-        }
-    }
-}
-
-
-#############################################################
-# Create Key Vault & KEK
-#############################################################
-# KEK cannot be created via ARM template
-$KeyVault = $('kv' + $VSE.DomainPrefixAbbreviation + $VSE.Environment + $VSE.Locations[0])
-$KeyVaultResourceGroup = $('rg-shared-' + $VSE.Environment + '-' + $VSE.Locations[0])
-
-New-AzKeyvault `
-    -Name $KeyVault `
-    -ResourceGroupName $KeyVaultResourceGroup `
-    -Location $VSE.Locations[0] `
-    -EnabledForDiskEncryption `
-    -EnabledForDeployment `
-    -EnabledForTemplateDeployment `
-    -DisableSoftDelete `
-    -WarningAction SilentlyContinue
-
-Set-AzKeyVaultAccessPolicy `
-    -VaultName $KeyVault `
-    -ResourceGroupName $KeyVaultResourceGroup `
-    -ObjectId $UserObjectId `
-    -PermissionsToKeys encrypt,decrypt,wrapKey,unwrapKey,sign,verify,get,list,create,update,import,delete,backup,restore,recover,purge `
-    -PermissionsToSecrets get,list,set,delete,backup,restore,recover,purge
-
-Add-AzKeyVaultKey `
-    -Name "DiskEncryption" `
-    -VaultName $KeyVault `
-    -Destination "Software"
-
-$KeyEncryptionKeyURL = (Get-AzKeyVaultKey `
-    -VaultName $KeyVault `
-    -Name 'DiskEncryption' `
-    -IncludeVersions | Where-Object {$_.Enabled -eq $true}).Id
-
-$VSE.Add("KeyEncryptionKeyURL", $KeyEncryptionKeyURL)
 
 
 #############################################################
---
+++ utilities/Disable-AdConnect.ps1
@@ -0,0 +1,19 @@
+﻿# TIP: Use the Global Admin account used to setup AD Connect
+
+Param(
+
+    [string] [Paramter(Mandatory=$true)] $TenantId
+
+)
+
+if(!(Get-Module -ListAvailable | Where-Object {$_.Name -eq 'MSOnline'}))
+{
+    Install-Module -Name MSOnline -AllowClobber -Force
+}
+
+if(!(Get-MsolDomain -ErrorAction SilentlyContinue))
+{
+    Connect-MsolService -AzureEnvironment AzureCloud
+}
+
+Set-MsolDirSyncEnabled -TenantId $TenantId -EnableDirSync $false -Force

b5a457377fe151722bb362943740014dc12ca0ee
Jason Masten <jamasten@microsoft.com>
2020-09-10 22:40:34 -0400

    created function to add my home pip to nsg

---
+++ utilities/functions.ps1
@@ -0,0 +1,7 @@
+﻿function Get-PublicIpAddress
+{
+    $url = "http://checkip.dyndns.com"   
+    $webclient = New-Object System.Net.WebClient  
+    $response = $webclient.DownloadString($url).Trim()
+    $ip = (Select-String -InputObject $response -Pattern '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}').Matches.Value
+}

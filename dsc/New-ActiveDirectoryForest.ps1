0b7b56a17ef7a0a6db1f057ec7d6faca7b6f5364
Jason Masten <jamasten@microsoft.com>
2020-09-13 14:47:04 -0400

    testing aa creds

--- dsc/New-ActiveDirectoryForest.ps1
+++ dsc/New-ActiveDirectoryForest.ps1
@@ -16,7 +16,8 @@ configuration ActiveDirectoryForest
     ) 
     
     Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
-    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($VmUsername)", $VmPassword)
+    #[System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($VmUsername)", $VmPassword)
+    $DomainCreds = Get-AutomationPSCredential 'AdminCred'
     $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
     $InterfaceAlias=$($Interface.Name)
 

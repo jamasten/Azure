e17106b255a6d3449841df313b317f18a32fd85f
Jason Masten <jamasten@microsoft.com>
2020-09-16 11:45:22 -0400

    add AD users to dsc config

--- dsc/configurations/ActiveDirectoryForest.ps1
+++ dsc/configurations/ActiveDirectoryForest.ps1
@@ -13,6 +13,36 @@ configuration ActiveDirectoryForest
     Import-DscResource -ModuleName xDnsServer -ModuleVersion '1.16.0.0'
 
     $DomainCreds = Get-AutomationPSCredential 'Administrator'
+    $Users = @(
+        @{
+            "FirstName" = "Cereal";
+            "LastName" = "Killer";
+        },
+        @{
+            "FirstName" = "Lord";
+            "LastName" = "Nikon";
+        },
+        @{
+            "FirstName" = "Crash";
+            "LastName" = "Override";
+        },
+        @{
+            "FirstName" = "Phantom";
+            "LastName" = "Phreak";
+        },
+        @{
+            "FirstName" = "Acid";
+            "LastName" = "Burn";
+        },
+        @{
+            "FirstName" = "Zero";
+            "LastName" = "Cool";
+        },
+        @{
+            "FirstName" = "The";
+            "LastName" = "Plague";
+        }
+    )
 
     Node localhost
     {
@@ -40,12 +70,48 @@ configuration ActiveDirectoryForest
             DependsOn = "[WindowsFeature]DNS"
 	    }
 
-        DnsServerAddress DnsServerAddress 
+        DnsServerAddress DnsServer 
         { 
             Address        = '127.0.0.1' 
             InterfaceAlias = 'Ethernet'
             AddressFamily  = 'IPv4'
 	        DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xDnsServerDiagnostics EnableDnsDiagnostics
+        {
+            Name = 'EnableDnsDiagnostics'
+            Answers = $true
+            EnableLogFileRollover = $true
+            FullPackets = $true
+            EnableLoggingForLocalLookupEvent = $true
+            EnableLoggingForPluginDllEvent = $true
+            EnableLoggingForRecursiveLookupEvent = $true
+            EnableLoggingForRemoteServerEvent = $true
+            EnableLoggingForServerStartStopEvent = $true
+            EnableLoggingForTombstoneEvent = $true
+            EnableLoggingForZoneDataWriteEvent = $true
+            EnableLoggingForZoneLoadingEvent = $true
+            Notifications = $true
+            Queries = $true
+            QuestionTransactions = $true
+            SaveLogsToPersistentStorage = $true
+            ReceivePackets = $true
+            SendPackets = $true
+            TcpPackets = $true
+            UdpPackets = $true
+            UnmatchedResponse = $true
+            Update = $true
+            WriteThrough = $true
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xDnsServerForwarder Azure
+        {
+            IsSingleInstance = 'Yes'
+            IPAddresses = '168.63.129.16'
+            UseRootHint = $true
+            DependsOn = "[WindowsFeature]DNS"
         }
 
         WindowsFeature ADDSInstall 
@@ -78,39 +144,24 @@ configuration ActiveDirectoryForest
 	  	    DependsOn = "[WindowsFeature]ADDSInstall"
         }
 
-        xDnsServerDiagnostics EnableDnsDiagnostics
+        foreach($User in $Users)
         {
-            Name = 'EnableDnsDiagnostics'
-            Answers = $true
-            EnableLogFileRollover = $true
-            FullPackets = $true
-            EnableLoggingForLocalLookupEvent = $true
-            EnableLoggingForPluginDllEvent = $true
-            EnableLoggingForRecursiveLookupEvent = $true
-            EnableLoggingForRemoteServerEvent = $true
-            EnableLoggingForServerStartStopEvent = $true
-            EnableLoggingForTombstoneEvent = $true
-            EnableLoggingForZoneDataWriteEvent = $true
-            EnableLoggingForZoneLoadingEvent = $true
-            Notifications = $true
-            Queries = $true
-            QuestionTransactions = $true
-            SaveLogsToPersistentStorage = $true
-            ReceivePackets = $true
-            SendPackets = $true
-            TcpPackets = $true
-            UdpPackets = $true
-            UnmatchedResponse = $true
-            Update = $true
-            WriteThrough = $true
-        }
-
-        xDnsServerForwarder Azure
-        {
-            IsSingleInstance = 'Yes'
-            IPAddresses = '168.63.129.16'
-            UseRootHint = $true
-            DependsOn = "[WindowsFeature]DNS"
+            ADUser $($User.FirstName + $User.LastName)
+            {
+                Ensure = 'Present'
+                Password = $DomainCreds
+                PasswordNeverResets = $false
+                DomainName = $Domain
+                CannotChangePassword = $true
+                ChangePasswordAtLogon = $false 
+                DisplayName = $User.FirstName + ' ' + $User.LastName
+                Enabled = $true
+                GivenName = $User.FirstName
+                PasswordNeverExpires = $true
+                Surname = $User.LastName
+                UserName = ($User.FirstName + $User.LastName).ToLower()
+                UserPrincipalName = ($User.FirstName + $User.LastName + '@' + $Domain).ToLower()
+            }
         }
     }
 }

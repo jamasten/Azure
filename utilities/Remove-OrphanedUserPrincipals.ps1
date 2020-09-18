811cb31f9d30ce3c267a621c5cc7f1bbac7f7a97
Jason Masten <jamasten@microsoft.com>
2020-09-17 23:55:46 -0400

    created script

---
+++ utilities/Remove-OrphanedUserPrincipals.ps1
@@ -0,0 +1,16 @@
+﻿$Users = @(
+    'acidburn@jasonmasten.com',
+    'adconnect@jasonmasten.com',
+    'cerealkiller@jasonmasten.com',
+    'crashoverride@jasonmasten.com',
+    'lordnikon@jasonmasten.com',
+    'phantomphreak@jasonmasten.com',
+    'theplague@jasonmasten.com',
+    'zerocool@jasonmasten.com'
+)
+$Users += Get-AzADUser | Where-Object {$_.UserPrincipalName -like "Sync_*"} | Select-Object -ExpandProperty UserPrincipalName
+
+foreach($User in $Users)
+{
+    Remove-AzADUser -UserPrincipalName $User -Force 
+}

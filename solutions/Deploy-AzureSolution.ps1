058c565f2f265ea1db5005576293e58bdbb5d87b
Jason Masten <jamasten@microsoft.com>
2020-10-01 00:04:17 -0400

    created script to deploy solutions

---
+++ solutions/Deploy-AzureSolution.ps1
@@ -0,0 +1,47 @@
+﻿Param(
+
+    [parameter(Mandatory=$true)]
+    [string]$ResourceGroup,
+  
+    [parameter(Mandatory=$true)]
+    [string]$SubscriptionId,
+
+    [parameter(Mandatory=$true)]
+    [string]$TemplateFile
+)
+
+#############################################################
+# Authenticate to Azure
+#############################################################
+if(!(Get-AzSubscription | Where-Object {$_.Id -eq $SubscriptionId}))
+{
+    Connect-AzAccount `
+        -Subscription $SubscriptionId `
+        -UseDeviceAuthentication
+}
+
+
+#############################################################
+# Set Subscription Context
+#############################################################
+if(!(Get-AzContext | Where-Object {$_.Subscription.Id -eq $SubscriptionId}))
+{
+    Set-AzContext -Subscription $SubscriptionId
+}
+
+
+#############################################################
+# Deployment
+#############################################################
+try 
+{
+    New-AzResourceGroupDeployment `
+        -ResourceGroupName $ResourceGroup `
+        -TemplateFile $TemplateFile `
+        -ErrorAction Stop
+}
+catch 
+{
+    Write-Host "Deployment Failed: $Name"
+    $_ | Select-Object *
+}

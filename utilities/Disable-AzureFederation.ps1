5899936bd1710614d90a7511e23bae2bc91a441f
Jason Masten <jamasten@microsoft.com>
2020-10-22 16:49:37 -0400

    created file

---
+++ utilities/Disable-AzureFederation.ps1
@@ -0,0 +1,19 @@
+﻿# TIP: Use the Global Admin account used to setup AD Connect
+
+Param(
+
+    [string] [Paramter(Mandatory=$true)] $Domain
+
+)
+
+if(!(Get-Module -ListAvailable | Where-Object {$_.Name -eq 'MSOnline'}))
+{
+    Install-Module -Name MSOnline -AllowClobber -Force
+}
+
+if(!(Get-MsolDomain -ErrorAction SilentlyContinue))
+{
+    Connect-MsolService -AzureEnvironment AzureCloud
+}
+
+Set-MsolDomainAuthentication -DomainName $Domain -Authentication Managed

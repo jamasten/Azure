3620b56e45e55719aa432060b36ca791b1c66715
Jason Masten <jamasten@microsoft.com>
2020-10-27 20:55:39 -0400

    created file

---
+++ utilities/Clean-AzureSubscription.ps1
@@ -0,0 +1,13 @@
+﻿# Remove Azure AD Users after removal of AD Connect
+
+
+
+# Remove Azure AD Groups after removal of AD Connect
+$Groups = @(
+    'ADSyncAdmins',
+    'ADSyncBrowse',
+    'ADSyncOperators',
+    'ADSyncPasswordSet',
+    'DnsAdmins',
+    'DnsUpdateProxy'
+)

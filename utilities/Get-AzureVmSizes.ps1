c40c3d42cec9079bcdc70d1d247f8186c0027f86
Jason Masten <jamasten@microsoft.com>
2020-11-19 11:07:17 -0500

    created script

---
+++ utilities/Get-AzureVmSizes.ps1
@@ -0,0 +1,18 @@
+﻿# The purpose of this script is to generate all VM Sku's in JSON format
+# to be used in an ARM template for an "allowed values" array in a parameter
+
+$Regions = (Get-AzLocation).Location
+$AllSizeData = @()
+foreach($Region in $Regions)
+{
+    $AllSizeData += (Get-AzVMSize -Location $Region -ErrorAction SilentlyContinue).Name
+}
+
+$FilteredData = $AllSizeData | Select-Object -Unique | sort
+$FormattedData = @()
+foreach($Size in $FilteredData)
+{
+    $FormattedData += '"' + $Size + '",'
+}
+
+$FormattedData

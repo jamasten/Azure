17cbd732a07c45ff30e8499d99ff092e47e027b0
Jason Masten <jamasten@microsoft.com>
2021-01-13 20:18:25 -0500

    created file

---
+++ utilities/wvd/Setup-FSLogix.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = "Stop"
+
+$Path = 'C:\Temp'
+
+New-Item -Path C:\ -Name 'Temp' -ItemType Directory
+
+Set-Location -Path $Path
+
+Invoke-WebRequest -UseBasicParsing -Uri 'https://aka.ms/fslogix_download' -OutFile $($Path + '\FSlogix.zip')
+
+Expand-Archive -Path $($Path + '\FSlogix.zip')
+
+Start-Process -FilePath 'C:\Temp\FSlogix\x64\Release\FSLogixAppsSetup.exe' -ArgumentList "/install","/quiet" -Wait | Out-Null
+
+Remove-Item -Path $Path -Recurse -Force

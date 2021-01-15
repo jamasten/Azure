d50db361739d5b7216d79ab93658559532ea5967
Jason Masten <jamasten@microsoft.com>
2021-01-14 22:01:44 -0500

    new folder idempotent & removed cmdlet

--- utilities/wvd/Setup-FSLogix.ps1
+++ utilities/wvd/Setup-FSLogix.ps1
@@ -2,16 +2,18 @@
 
 $Path = 'C:\Temp'
 
-New-Item -Path C:\ -Name 'Temp' -ItemType Directory
+New-Item -Path C:\ -Name 'Temp' -ItemType Directory -Force
 
 Set-Location -Path $Path
 
-Invoke-WebRequest -UseBasicParsing -Uri 'https://aka.ms/fslogix_download' -OutFile $($Path + '\FSlogix.zip')
+$Installed = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq 'Microsoft FSLogix Apps'}
+if($Installed.Count -eq 0)
+{
+    Invoke-WebRequest -UseBasicParsing -Uri 'https://aka.ms/fslogix_download' -OutFile $($Path + '\FSlogix.zip')
 
-Expand-Archive -Path $($Path + '\FSlogix.zip')
+    Expand-Archive -Path $($Path + '\FSlogix.zip')
 
-Start-Process -FilePath 'C:\Temp\FSlogix\x64\Release\FSLogixAppsSetup.exe' -ArgumentList "/install","/quiet" -Wait | Out-Null
-
-Remove-Item -Path $Path -Recurse -Force
+    Start-Process -FilePath 'C:\Temp\FSlogix\x64\Release\FSLogixAppsSetup.exe' -ArgumentList "/install","/quiet" -Wait | Out-Null
+}
 
 Start-Process -FilePath 'C:\Windows\System32\gpupdate.exe' -ArgumentList "/force" -Wait | Out-Null

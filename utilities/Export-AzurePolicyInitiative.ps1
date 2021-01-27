8e430932a26adbdbf799546dff38ee338c75cc3b
Jason Masten <jamasten@microsoft.com>
2021-01-27 14:03:48 -0500

    added notes & extra error handling

--- utilities/Export-AzurePolicyInitiative.ps1
+++ utilities/Export-AzurePolicyInitiative.ps1
@@ -1,18 +1,51 @@
-﻿Param(
+﻿<#
+.SYNOPSIS
 
+Exports an Azure Policy Initiative Definition
+
+.DESCRIPTION
+
+The Export-AzurePolicyInitiative.ps1 script exports an initiative definition.  This script will first 
+create a directory based on the display name of the initiative on the user's Desktop. Then it exports 
+out the initiative defintion properties to either a CSV file or JSON.  Afterwards, the policy 
+definitions are exported out using CSV and JSON files.
+
+.PARAMETER InitiativeName
+
+Specifies the Display Name of the initiative defintion to export.
+
+.INPUTS
+
+None. You cannot pipe objects to Export-AzurePolicyInitiative.ps1.
+
+.OUTPUTS
+
+The OUTPUT from this script will be a directory on your filesystem that contains the initiative definition code,
+along with all the policy definition code the initiative is dependent on.
+
+.EXAMPLE
+
+PS> .\Export-AzurePolicyInitiative.ps1 -InitiativeName "FedRAMP High"
+#>
+
+Param(
+
+    # Specifies the Display Name of the initiative defintion to export
     [parameter(Mandatory=$true)]
     [string]$InitiativeName
 
 )
 
-# Get the initiative's properties using the name
+# Gets the initiative definition's properties using the display name
 $Initiative = Get-AzPolicySetDefinition | Where-Object {$_.Properties.DisplayName -eq $InitiativeName}
 
-# Define the working directory and create a folder for the initiative
+# Defines the working directory
 $InitiativePath = $HOME + '\Desktop\' + $InitiativeName
+
+# Creates a folder to store the initiative definition as code
 New-Item -Path $InitiativePath -ItemType Directory
 
-# Download Initiative Data
+# Creates a CSV file with the Description, DisplayName, and Name values from the initiative definition properties
 $InitiativeData = [pscustomobject][ordered]@{
     Description = $Initiative.Properties.Description
     DisplayName = $Initiative.Properties.DisplayName
@@ -20,17 +53,17 @@ $InitiativeData = [pscustomobject][ordered]@{
 }
 $InitiativeData | Export-Csv -Path ($InitiativePath + '\data.csv') -NoTypeInformation
 
-# Download Initiative Metadata
+# Creates a json file of the initiative definition "Metadata"
 $Initiative.Properties.Metadata | ConvertTo-Json -Depth 100 | Out-File -FilePath ($InitiativePath + '\metadata.json')
 
-# Download Policy Definitions
+# Creates a json file of the initiative definition "Policy Definitions"
 $Definitions = $([System.Text.RegularExpressions.Regex]::Unescape(($Initiative.Properties.PolicyDefinitions | ConvertTo-Json -Depth 100)))
 $Definitions | Out-File -FilePath ($InitiativePath + '\policyDefinitions.json')
 
-# Download Policy Definition Groups
+# Creates a json file of the initiative definition "Policy Definition Groups"
 $Initiative.Properties.PolicyDefinitionGroups | ConvertTo-Json -Depth 100 | Out-File -FilePath ($InitiativePath + '\policyDefinitionGroups.json')
 
-# Download Policy Parameters
+# Creates a json file of the initiative definition "Parameters"
 $Initiative.Properties.Parameters | ConvertTo-Json -Depth 100 | Out-File -FilePath ($InitiativePath + '\parameters.json')
 
 # Download Policy Definitions to Initiative folder
@@ -41,7 +74,7 @@ foreach($Policy in $Initiative.Properties.PolicyDefinitions)
     New-Item -Path $PolicyPath -ItemType Directory
     $Policy = Get-AzPolicyDefinition -Name $Name
     
-    # Download policy data to CSV
+    # Creates a CSV file with the Description, DisplayName, and Mode values from the policy definition properties
     $Data = [pscustomobject][ordered]@{
         Description = $Policy.Properties.Description
         DisplayName = $Policy.Properties.DisplayName
@@ -49,13 +82,13 @@ foreach($Policy in $Initiative.Properties.PolicyDefinitions)
     }
     $Data | Export-Csv -Path ($PolicyPath + '\data.csv') -NoTypeInformation
 
-    # Download Policy Metadata
+    # Creates a json file of the policy definition metadata
     $Policy.Properties.Metadata | ConvertTo-Json -Depth 100 | Out-File -FilePath ($PolicyPath + '\metadata.json')
 
-    # Download Policy Rule
+    # Creates a json file of the policy definition rule while escaping single quotes in the code
     $Rule = $([System.Text.RegularExpressions.Regex]::Unescape(($Policy.Properties.PolicyRule | ConvertTo-Json -Depth 100)))
     $Rule | Out-File -FilePath ($PolicyPath + '\rule.json')
 
-    # Download Policy Parameters
+    # Creates a json file of the policy definition parameters
     $Policy.Properties.Parameters | ConvertTo-Json -Depth 100 | Out-File -FilePath ($PolicyPath + '\parameters.json')
 }
--- utilities/Import-AzurePolicyInitiative.ps1
+++ utilities/Import-AzurePolicyInitiative.ps1
@@ -1,19 +1,64 @@
-﻿Param(
+﻿<#
+.SYNOPSIS
 
+Imports an Azure Policy Initiative Definition
+
+.DESCRIPTION
+
+The Import-AzurePolicyInitiative.ps1 script imports an initiative definition that
+was exported using Jason Masten's Export-AzurePolicyInitiative.ps1 script.
+
+This script will first create the required policy definitions for the initiative.
+Once the policy definitions exist, the initiative definition can and will be created.
+
+.PARAMETER Path
+
+Specifies the location of the exported initiative definition that will be imported.
+
+.INPUTS
+
+None. You cannot pipe objects to Import-AzurePolicyInitiative.ps1.
+
+.OUTPUTS
+
+The OUTPUT from this script will contain the data returned from running the New-AzPolicySetDefiniton cmdlet.
+
+.EXAMPLE
+
+PS> .\Import-AzurePolicyInitiative.ps1 -Path "C:\JasonMasten\Desktop\FedRAMP High"
+#>
+
+Param(
+
+    # Specifies the location of the exported initiative definition that will be imported
     [parameter(Mandatory=$true)]
     [string]$Path
 
 )
 
-# Importing the Policy Definitions
+########################################################################################
+# Import Policy Definitions
+########################################################################################
+
+# The $Policies variable is set using all the policy definition names that were exported
+# using the Export-AzurePolicyInitiative.ps1 script
 $Policies = (Get-ChildItem -Path $Path -Directory).Name
+
+# Use the policy name to get all the required data about the policy definition
 foreach($Policy in $Policies)
 {
+    # Sets the path to the policy on the file system
     $PolicyPath = $Path + '\' + $Policy
+    
+    # Imports the Description, DisplayName, and Mode for the policy definiton
     $PolicyData = Import-Csv -Path ($PolicyPath + '\data.csv')
     try
     {
+        # Checks the status of the parameters file to conditionally create a new policy definition
         $Test = Get-Content -Path ($PolicyPath + '\parameters.json')
+        
+        # If the paramaters.json file has content then the "parameter" parameter is specified
+        # with the cmdlet.
         if($Test)
         {
             New-AzPolicyDefinition `
@@ -27,6 +72,9 @@ foreach($Policy in $Policies)
                 -Verbose `
                 -ErrorAction Stop | Out-Null
         }
+
+        # If the parameters.json file is empty then the "parameter" parameter is not specified
+        # with the cmdlet.
         else
         {
             New-AzPolicyDefinition `
@@ -42,35 +90,55 @@ foreach($Policy in $Policies)
     }
     catch
     {
-        Write-Host "$Policy" -ForegroundColor Red
+        Write-Host "$Policy :: FAILED" -ForegroundColor Red
+        $_ | Select-Object *
     }
 }
 
-# Importing the Initiative Definition
+
+########################################################################################
+# Import Initiative Definition
+########################################################################################
+
+# Imports the Description, DisplayName, and Name for the initiative definiton
 $InitiativeData = Import-Csv -Path ($Path + '\data.csv')
+
+# Checks the status of the policyDefinitionGroups.json file to conditionally create a new
+# initiative definition
 $TestDefGroups = Get-Content -Path ($Path + '\policyDefinitionGroups.json')
-if($TestDefGroups)
-{
-    New-AzPolicySetDefinition `
-        -Name $InitiativeData.Name `
-        -DisplayName $InitiativeData.DisplayName `
-        -Description $InitiativeData.Description `
-        -Metadata ($Path + '\metadata.json') `
-        -PolicyDefinition ($Path + '\policyDefinitions.json') `
-        -Parameter ($Path + '\parameters.json') `
-        -GroupDefinition ($Path + '\policyDefinitionGroups.json') `
-        -Verbose `
-        -ErrorAction Stop
+try
+{   
+    # If the policyDefinitionGroups.json file has content then the "GroupDefinition" 
+    # parameter is specified with the cmdlet.
+    if($TestDefGroups)
+    {
+        New-AzPolicySetDefinition `
+            -Name $InitiativeData.Name `
+            -DisplayName $InitiativeData.DisplayName `
+            -Description $InitiativeData.Description `
+            -Metadata ($Path + '\metadata.json') `
+            -PolicyDefinition ($Path + '\policyDefinitions.json') `
+            -Parameter ($Path + '\parameters.json') `
+            -GroupDefinition ($Path + '\policyDefinitionGroups.json') `
+            -Verbose `
+            -ErrorAction Stop
+    }
+    # If the policyDefinitionGroups.json file is empty then the "GroupDefinition" 
+    # parameter is not specified with the cmdlet.
+    else
+    {
+        New-AzPolicySetDefinition `
+            -Name $InitiativeData.Name `
+            -DisplayName $InitiativeData.DisplayName `
+            -Description $InitiativeData.Description `
+            -Metadata ($Path + '\metadata.json') `
+            -PolicyDefinition ($Path + '\policyDefinitions.json') `
+            -Parameter ($Path + '\parameters.json') `
+            -Verbose `
+            -ErrorAction Stop
+    }
 }
-else
+catch
 {
-    New-AzPolicySetDefinition `
-        -Name $InitiativeData.Name `
-        -DisplayName $InitiativeData.DisplayName `
-        -Description $InitiativeData.Description `
-        -Metadata ($Path + '\metadata.json') `
-        -PolicyDefinition ($Path + '\policyDefinitions.json') `
-        -Parameter ($Path + '\parameters.json') `
-        -Verbose `
-        -ErrorAction Stop
+    $_ | Select-Object *
 }

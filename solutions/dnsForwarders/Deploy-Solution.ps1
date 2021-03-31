8b597588b82c07d7c6944b89c6089004c399a263
Jason Masten <jamasten@microsoft.com>
2021-03-30 23:22:56 -0400

    removed unnecessary file

--- solutions/dnsForwarders/Deploy-Solution.ps1
+++
@@ -1,6 +0,0 @@
-﻿New-AzResourceGroupDeployment `
-    -Name (Get-Date -F 'yyyyMMdd_hhmmss') `
-    -ResourceGroupName rg-dns-p-eastus `
-    -TemplateFile "C:\Users\jamasten\GitHub\Azure\solutions\dnsForwarders\template.json" `
-    -SubnetId '/subscriptions/3764b123-4849-4395-8e6e-ca6d68d8d4b4/resourceGroups/rg-network-d-eastus/providers/Microsoft.Network/virtualNetworks/vnet-d-eastus/subnets/snet-servers-d-eastus' `
-    -VmUsername 'rebukem'

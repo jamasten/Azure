820ae557d9b1f2e50a133787c673bae8bd2f5d7b
Jason Masten <jamasten@microsoft.com>
2021-04-01 11:38:59 -0400

    changed error action

--- solutions/wvd/scripts/New-ADStorageAccount.ps1
+++ solutions/wvd/scripts/New-ADStorageAccount.ps1
@@ -49,7 +49,7 @@ $SPN = 'cifs/' + $StorageAccountName + $Suffix
 
 $Description = "Computer account object for Azure storage account $($StorageAccountName)."
 
-$Test = Get-ADComputer -Identity $StorageAccountName -ErrorAction Ignore
+$Test = Get-ADComputer -Identity $StorageAccountName -ErrorAction SilentlyContinue
 if(!$Test)
 {
     try

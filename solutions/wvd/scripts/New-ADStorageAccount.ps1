568be51fee84ede41f3cb29cb15904e829ce0b79
Jason Masten <jamasten@microsoft.com>
2021-04-01 10:19:48 -0400

    added error handling & custom logging

--- solutions/wvd/scripts/New-ADStorageAccount.ps1
+++ solutions/wvd/scripts/New-ADStorageAccount.ps1
@@ -16,39 +16,71 @@ param
     [String]$StorageAccountName
 )
 
+function Write-Log
+{
+    param(
+        [parameter][string]$Message,
+        [parameter][string]$Type
+    )
+    $Path = 'C:\cse.log'
+    if(Test-Path -Path C:\cse.log)
+    {
+        New-Item -Path C:\ -Name cse.log
+    }
+    $Timestamp = Get-Date -Format 'mm/dd/yyyy HH:mm:ss.ff'
+    $Entry = '[' + $Timestamp + '] [' + $Type + '] ' + $Message
+    $Entry | Out-File -FilePath $Path -Append
+}
+
 $Suffix = switch($Environment)
 {
     AzureCloud {'.file.core.windows.net'}
     AzureUSGovernment {'.file.core.usgovcloudapi.net'}
 }
+Write-Log -Message "Storage Account Suffix = $Suffix" -Type INFO
 
-$Password = ConvertTo-SecureString -String $Key -AsPlainText -Force
+$Password = ConvertTo-SecureString -String $Key -AsPlainText -Force -ErrorAction Stop
+Write-Log -Message "Secure string conversion succeeded" -Type INFO
 
 $SPN = 'cifs/' + $StorageAccountName + $Suffix
 
-$Test = (Get-ADComputer -Identity $StorageAccountName).SID.Value
+$Test = Get-ADComputer -Identity $StorageAccountName -ErrorAction SilentlyContinue
 if(!$Test)
 {
-    if(!$OuPath)
+    try
     {
-        New-ADComputer `
-            -Name $StorageAccountName `
-            -ServicePrincipalNames $SPN `
-            -AccountPassword $Password `
-            -KerberosEncryptionType $KerberosEncryptionType
-    } 
-    else 
+        if(!$OuPath)
+        {
+            New-ADComputer `
+                -Name $StorageAccountName `
+                -ServicePrincipalNames $SPN `
+                -AccountPassword $Password `
+                -KerberosEncryptionType $KerberosEncryptionType `
+                -ErrorAction Stop
+        } 
+        else 
+        {
+            New-ADComputer `
+                -Name $StorageAccountName `
+                -ServicePrincipalNames $SPN `
+                -AccountPassword $Password `
+                -KerberosEncryptionType $KerberosEncryptionType `
+                -Path $OuPath `
+                -ErrorAction Stop
+        }
+        Write-Log -Message "Computer object creation succeeded" -Type INFO
+    }
+    catch
     {
-        New-ADComputer `
-            -Name $StorageAccountName `
-            -ServicePrincipalNames $SPN `
-            -AccountPassword $Password `
-            -KerberosEncryptionType $KerberosEncryptionType `
-            -Path $OuPath
+        Write-Log -Message "Failed to create computer object" -Type ERROR
+        $String = $_ | Select-Object * | Out-String
+        Write-Log -Message $String -Type ERROR
     }
 }
 
-$Domain = Get-ADDomain
-$ComputerSid = (Get-ADComputer -Identity $StorageAccountName).SID.Value
+$Domain = Get-ADDomain -ErrorAction Stop
+Write-Log -Message "Domain info collection succeeded" -Type INFO
+$ComputerSid = (Get-ADComputer -Identity $StorageAccountName -ErrorAction Stop).SID.Value
+Write-Log -Message "Computer object info collection succeeded" -Type INFO
 
 Write-Host ",$($Domain.ObjectGUID),$($Domain.DomainSID),$($ComputerSid),"

99df8a8a1ed03dd93d512e230fb13c8d24028a85
Jason Masten <jamasten@microsoft.com>
2021-04-02 14:58:05 -0400

    added file

---
+++ utilities/Get-AzureVmExtensionVersions.ps1
@@ -0,0 +1,11 @@
+﻿# Find the available extension publishers in your location
+$Location = 'eastus'
+Get-AzVmImagePublisher -Location $Location
+
+# Find the available extension types in your location
+$Publisher = 'Microsoft.Compute'
+Get-AzVMExtensionImageType -Location $Location -PublisherName $Publisher
+
+# Get the extension versions for the specified type in your location
+$Type = 'CustomScriptExtension'
+(Get-AzVMExtensionImage -Location $Location -PublisherName Microsoft.Compute -Type $Type).Version

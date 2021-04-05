54ad3369ec8f254ee92b85a242300ddaf5728e9e
Jason Masten <jamasten@microsoft.com>
2021-04-05 00:00:13 -0400

    updated fslogix settings

--- solutions/wvd/scripts/Add-FslogixSettings.ps1
+++ solutions/wvd/scripts/Add-FslogixSettings.ps1
@@ -18,8 +18,8 @@ $Suffix = switch($Environment)
 $FileShare = '\\' + $StorageAccountName + $Suffix + '\' + $HostPoolName
 
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name Enabled -PropertyType DWord -Value 1
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name DeleteLocalProfileWhenVHDShouldApply -PropertyType DWord -Value 1
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name DeleteLocalProfileWhenVHDShouldApply -PropertyType DWord -Value 0
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name FlipFlopProfileDirectoryName -PropertyType DWord -Value 1
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name PreventLoginWithFailure -PropertyType DWord -Value 1
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name PreventLoginWithTempProfile -PropertyType DWord -Value 1
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name PreventLoginWithFailure -PropertyType DWord -Value 0
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name PreventLoginWithTempProfile -PropertyType DWord -Value 0
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name VHDLocations -PropertyType MultiString -Value $FileShare

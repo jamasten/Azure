64d30800559941d89719137d245fbb21ff67b0e4
Jason Masten <jamasten@microsoft.com>
2021-04-04 22:27:34 -0400

    added ntfs permissions on Azure file share

---
+++ solutions/wvd/scripts/Set-FileSharePermissions.ps1
@@ -0,0 +1,34 @@
+﻿param 
+( 
+    [Parameter(Mandatory)]
+    [String]$Environment,
+
+    [Parameter(Mandatory)]
+    [String]$HostPoolName,
+
+    [Parameter(Mandatory)]
+    [String]$Netbios,
+
+    [Parameter(Mandatory)]
+    [String]$SecurityPrincipalName,
+
+    [Parameter(Mandatory)]
+    [String]$StorageAccountName
+)
+
+$Suffix = switch($Environment)
+{
+    AzureCloud {'.file.core.windows.net'}
+    AzureUSGovernment {'.file.core.usgovcloudapi.net'}
+}
+$FileShare = '\\' + $StorageAccountName + $Suffix + '\' + $HostPoolName
+$Group = $Netbios + '\' + $SecurityPrincipalName
+
+New-PSDrive -Name Z -PSProvider FileSystem -Root $FileShare -Persist -ErrorAction Stop
+
+Start-Process icacls -ArgumentList "Z: /grant $($Group):(M)" -Wait -NoNewWindow -PassThru -ErrorAction Stop
+Start-Process icacls -ArgumentList 'Z: /grant "Creator Owner":(OI)(CI)(IO)(M)' -Wait -NoNewWindow -PassThru -ErrorAction Stop
+Start-Process icacls -ArgumentList 'Z: /remove "Authenticated Users"' -Wait -NoNewWindow -PassThru -ErrorAction Stop
+Start-Process icacls -ArgumentList 'Z: /remove "Builtin\Users"' -Wait -NoNewWindow -PassThru -ErrorAction Stop
+
+Remove-PSDrive -Name Z -PSProvider FileSystem -Force -ErrorAction Stop

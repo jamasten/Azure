5b7762985874f15a2049c35b5445942b00b76b82
Jason Masten <jamasten@microsoft.com>
2021-05-13 09:53:37 -0400

    added script

---
+++ utilities/Fix-WvdDriveRedirection.ps1
@@ -0,0 +1,8 @@
+﻿$ResourceGroupName = ""
+$Name = ""
+
+$CurrentProperties = (Get-AzWvdHostPool -ResourceGroupName $ResourceGroupName -Name $Name ).CustomRdpProperty
+
+$NewProperties = $CurrentProperties + "drivestoredirect:s:;"
+
+Update-AzWvdHostPool -ResourceGroupName $ResourceGroupName -Name $Name -CustomRdpProperty $NewProperties

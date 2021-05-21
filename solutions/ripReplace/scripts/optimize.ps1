5aa5a92f8ede38b1f7be3183138de43c605cdd9a
Jason Masten <jamasten@microsoft.com>
2021-05-20 22:51:22 -0400

    add script for cse extension

---
+++ solutions/ripReplace/scripts/optimize.ps1
@@ -0,0 +1,43 @@
+# Author       : Dean Cefola
+# Creation Date: 07-09-2020
+# Usage        : Windows Virtual Desktop Optimization Script
+# All code that this script executes is created and provided by THE VDI GUYS
+# You can download the code here  --  https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool
+#********************************************************************************
+# Date                         Version      Changes
+#------------------------------------------------------------------------
+# 05/20/2021                     1.1        Removed WindowsVersion parameter (by Jason Masten)
+# 07/09/2020                     1.0        Intial Version
+#*********************************************************************************
+
+
+################################
+#    Download WVD Optimizer    #
+################################
+New-Item -Path C:\ -Name Optimize -ItemType Directory -ErrorAction SilentlyContinue
+$LocalPath = "C:\Optimize\"
+$WVDOptimizeURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
+$WVDOptimizeInstaller = "Windows_10_VDI_Optimize-master.zip"
+Invoke-WebRequest `
+    -Uri $WVDOptimizeURL `
+    -OutFile "$Localpath$WVDOptimizeInstaller"
+
+
+###############################
+#    Prep for WVD Optimize    #
+###############################
+Expand-Archive `
+    -LiteralPath "C:\Optimize\Windows_10_VDI_Optimize-master.zip" `
+    -DestinationPath "$Localpath" `
+    -Force `
+    -Verbose
+Set-Location -Path C:\Optimize\Virtual-Desktop-Optimization-Tool-master
+
+
+#################################
+#    Run WVD Optimize Script    #
+#################################
+New-Item -Path C:\Optimize\ -Name install.log -ItemType File -Force
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+add-content c:\Optimize\install.log "Starting Optimizations"  
+.\Win10_VirtualDesktop_Optimize.ps1 -Restart -Verbose

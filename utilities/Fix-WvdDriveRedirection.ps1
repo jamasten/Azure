ee6a403b1d9b4c1bfcd26d29a4115d969ef358a4
Jason Masten <jamasten@microsoft.com>
2021-05-22 09:11:38 -0400

    added param block

--- utilities/Fix-WvdDriveRedirection.ps1
+++ utilities/Fix-WvdDriveRedirection.ps1
@@ -1,5 +1,13 @@
-﻿$ResourceGroupName = ""
-$Name = ""
+﻿[Cmdletbinding()]
+Param(
+
+[parameter(Mandatory)]
+[string]$Name,
+
+[parameter(Mandatory)]
+[string]$ResourceGroupName
+
+)
 
 $CurrentProperties = (Get-AzWvdHostPool -ResourceGroupName $ResourceGroupName -Name $Name ).CustomRdpProperty
 

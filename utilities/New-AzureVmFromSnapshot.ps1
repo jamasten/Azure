3e8a9fe7a0bd576166fac547474b5d04deee3106
Jason Masten <jamasten@microsoft.com>
2021-05-29 00:15:11 -0400

    made pip conditional; added hyperv generation

--- utilities/New-AzureVmFromSnapshot.ps1
+++ utilities/New-AzureVmFromSnapshot.ps1
@@ -1,79 +1,94 @@
 [Cmdletbinding()]
 Param(
 
-    #Provide the subscription Id
+    # Provide the HypverV Generation for the VM Disk
     [parameter(Mandatory)]
+    [ValidateSet("V1", "V2")]
     [string]
-    $subscriptionId,
+    $HyperVGeneration,
 
-    #Provide the name of your resource group
+    # Determine whether you want to deploy a Public IP Address for your VM
+    [parameter(Mandatory=$false)]
+    [switch]
+    $PublicIpAddress,
+
+    # Provide the name of your resource group
     [parameter(Mandatory)]
     [string]
     $resourceGroupName,
 
-    #Provide the name of the snapshot that will be used to create OS disk
+    # Provide the naming suffix for each resource
+    [parameter(Mandatory)]
+    [string]
+    $resourceNameSuffix,
+
+    # Provide the name of the snapshot that will be used to create OS disk
     [parameter(Mandatory)]
     [string]
     $snapshotName,
 
-    #Provide the name of the OS disk that will be created using the snapshot
+    # Provide the name of the subnet that will be used for the network interface
     [parameter(Mandatory)]
     [string]
-    $osDiskName,
+    $subnetName,
 
-    #Provide the name of an existing virtual network where the virtual machine NIC will be added
+    # Provide the Subscription ID
     [parameter(Mandatory)]
     [string]
-    $virtualNetworkName,
+    $subscriptionId,
 
-    #Provide the name of the virtual machine
-    [parameter(Mandatory)]
-    [string]
-    $virtualMachineName,
-
-    #Provide the size of the virtual machine
-    #e.g. Standard_DS3
-    #Get all the vm sizes in a region using below script:
-    #e.g. Get-AzVMSize -Location westus
+    # Provide the size of the virtual machine
     [parameter(Mandatory)]
     [string]
     $virtualMachineSize,
 
-    #Provide the name of an existing virtual network resource group where the virtual machine NIC will be added
+    # Provide the name of an existing virtual network where the virtual machine NIC will be added
     [parameter(Mandatory)]
     [string]
-    $vnetResourceGroupName
+    $virtualNetworkName,
+
+    # Provide the name of an existing virtual network resource group where the virtual machine NIC will be added
+    [parameter(Mandatory)]
+    [string]
+    $virtualNetworkResourceGroupName
 
 )
 
-#Set the context to the subscription Id where Managed Disk will be created
+# Set the context to the subscription Id where Managed Disk will be created
 Select-AzSubscription -SubscriptionId $SubscriptionId
 
+# Get the snapshot
 $snapshot = Get-AzSnapshot -ResourceGroupName $resourceGroupName -SnapshotName $snapshotName
 
-$diskConfig = New-AzDiskConfig -Location $snapshot.Location -SourceResourceId $snapshot.Id -CreateOption Copy
+# Create the disk configuration
+$diskConfig = New-AzDiskConfig -Location $snapshot.Location -SourceResourceId $snapshot.Id -CreateOption Copy -HyperVGeneration $HyperVGeneration
 
-$disk = New-AzDisk -Disk $diskConfig -ResourceGroupName $resourceGroupName -DiskName $osDiskName
+# Create a new disk
+$disk = New-AzDisk -Disk $diskConfig -ResourceGroupName $resourceGroupName -DiskName ('disk-'+$resourceNameSuffix)
 
-#Initialize virtual machine configuration
-$VirtualMachine = New-AzVMConfig -VMName $virtualMachineName -VMSize $virtualMachineSize
+# Initialize virtual machine configuration
+$VirtualMachine = New-AzVMConfig -VMName ('vm-'+$resourceNameSuffix) -VMSize $virtualMachineSize
 
-#Use the Managed Disk Resource Id to attach it to the virtual machine. Please change the OS type to linux if OS disk has linux OS
+# Use the Managed Disk Resource Id to attach it to the virtual machine. Please change the OS type to linux if OS disk has linux OS
 $VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -ManagedDiskId $disk.Id -CreateOption Attach -Windows
 
-#Create a public IP for the VM
-$publicIp = New-AzPublicIpAddress -Name ($VirtualMachineName.ToLower()+'_ip') -ResourceGroupName $resourceGroupName -Location $snapshot.Location -AllocationMethod Dynamic
+# Create a public IP for the VM
+if($PublicIpAddress)
+{
+    $publicIp = New-AzPublicIpAddress -Name ('pip-'+$resourceNameSuffix) -ResourceGroupName $resourceGroupName -Location $snapshot.Location -AllocationMethod Dynamic
+}
 
-#Get the virtual network where virtual machine will be hosted
-$vnet = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $vnetResourceGroupName
+# Get the virtual network where virtual machine will be hosted
+$vnet = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $virtualNetworkResourceGroupName
 
-# Create NIC in the first subnet of the virtual network
-$nic = New-AzNetworkInterface -Name ($VirtualMachineName.ToLower()+'_nic') -ResourceGroupName $resourceGroupName -Location $snapshot.Location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $publicIp.Id
+# Create Network Interface in the first subnet of the virtual network
+$nic = New-AzNetworkInterface -Name ('nic-'+$resourceNameSuffix) -ResourceGroupName $resourceGroupName -Location $snapshot.Location -SubnetId ($vnet.Subnets | Where-Object {$_.Name -eq $subnetName}).Id -PublicIpAddressId $publicIp.Id
 
+# Add Network Interface to Virtual Machine
 $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $nic.Id
 
-#Disable boot diagnostics
-$VirtualMachine = Set-AzVMBootDiagnostic -VM $virtualMachineName -Disable
+# Disable Boot Diagnostics
+$VirtualMachine = Set-AzVMBootDiagnostic -VM $virtualMachine -Disable
 
-#Create the virtual machine with Managed Disk
+# Create the virtual machine with Managed Disk
 New-AzVM -VM $VirtualMachine -ResourceGroupName $resourceGroupName -Location $snapshot.Location

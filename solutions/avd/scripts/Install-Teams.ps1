29d5276acfc54cf36d9e0ac79c09d41bdf473bbc
Jason Masten <jamasten@microsoft.com>
2021-07-06 17:04:13 -0400

    updated quotes

--- solutions/avd/scripts/Install-Teams.ps1
+++ solutions/avd/scripts/Install-Teams.ps1
@@ -1,11 +1,11 @@
  # Set Registry Key & Setting for AVD Media Optimization
- New-Item -Path HKLM:\SOFTWARE\Microsoft -Name "Teams" 
- New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Teams -Name "IsWVDEnvironment" -Type "Dword" -Value "1"
+ New-Item -Path HKLM:\SOFTWARE\Microsoft -Name 'Teams'
+ New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Teams -Name 'IsWVDEnvironment' -PropertyType 'Dword' -Value 1
  
  # Install Visual C++
  $appName = 'teams'
  $drive = 'C:\'
- New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
+ New-Item -Path $drive -Name $appName  -ItemType 'Directory' -ErrorAction 'SilentlyContinue'
  $LocalPath = $drive + '\' + $appName 
  set-Location $LocalPath
  $visCplusURL = 'https://aka.ms/vs/16/release/vc_redist.x64.exe'

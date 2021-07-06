7b33c8368e0c3829115b357f0957d70d293ca639
Jason Masten <jamasten@microsoft.com>
2021-07-06 16:40:52 -0400

    added comments & quotes

--- solutions/avd/scripts/Add-FslogixSettings.ps1
+++ solutions/avd/scripts/Add-FslogixSettings.ps1
@@ -17,9 +17,20 @@ $Suffix = switch($Environment)
 }
 $FileShare = '\\' + $StorageAccountName + $Suffix + '\' + $HostPoolName
 
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name Enabled -PropertyType DWord -Value 1
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name DeleteLocalProfileWhenVHDShouldApply -PropertyType DWord -Value 0
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name FlipFlopProfileDirectoryName -PropertyType DWord -Value 1
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name PreventLoginWithFailure -PropertyType DWord -Value 0
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name PreventLoginWithTempProfile -PropertyType DWord -Value 0
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name VHDLocations -PropertyType MultiString -Value $FileShare
+# Enables FSLogix profile containers
+New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'Enabled' -PropertyType 'DWord' -Value 1
+
+# Deletes a local profile if it exists and matches the profile being loaded from VHD
+New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'DeleteLocalProfileWhenVHDShouldApply' -PropertyType 'DWord' -Value 0
+
+# The folder created in the FSLogix fileshare will begin with the username instead of the SID
+New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'FlipFlopProfileDirectoryName' -PropertyType 'DWord' -Value 1
+
+# Loads FRXShell if there's a failure attaching to, or using an existing profile VHD(X)
+New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'PreventLoginWithFailure' -PropertyType 'DWord' -Value 1
+
+# Loads FRXShell if it's determined a temp profile has been created
+New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'PreventLoginWithTempProfile' -PropertyType 'DWord' -Value 0
+
+# List of file system locations to search for the user's profile VHD(X) file
+New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'VHDLocations' -PropertyType 'MultiString' -Value $FileShare

701149f88753c975e59d09e39ee21cd936e3bb82
Jason Masten <jamasten@microsoft.com>
2021-07-07 21:35:23 -0400

    added common values

--- utilities/Get-AzureImageVersion.ps1
+++ utilities/Get-AzureImageVersion.ps1
@@ -1,7 +1,12 @@
-﻿$location = "eastus"
-$publisher = "MicrosoftWindowsServer"
-$offer = "WindowsServer"
-$sku = "2019-Datacenter-Core"
+﻿$location = 'eastus'
+$publisher = 'MicrosoftWindowsDesktop'
+#$publisher = 'MicrosoftWindowsServer'
+#$offer = 'WindowsServer'
+$offer = 'office-365'
+#$offer = 'windows-evd'
+#$offer = 'Windows-10'
+#$sku = '2019-Datacenter-Core'
+$sku = '21h1-evd-o365pp'
 (Get-AzVMImagePublisher -Location $location).PublisherName
 (Get-AzVMImageOffer -Location $location -PublisherName $publisher).Offer
 (Get-AzVMImageSku -Location $location -PublisherName $publisher -Offer $offer).Skus

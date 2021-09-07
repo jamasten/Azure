a49f0adbd4ecc20e3f84d9df4d99f7e9e7e31a20
Jason Masten <jamasten@microsoft.com>
2021-09-07 11:42:07 -0400

    added script

---
+++ utilities/Format-WindowsDisk.ps1
@@ -0,0 +1,1 @@
+﻿Get-Disk | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "DataDisk" -Confirm:$false

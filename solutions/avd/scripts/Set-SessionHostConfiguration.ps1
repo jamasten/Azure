00628cbf737b8f512fec4b01b41c8808060e6b5d
Jason Masten <jamasten@microsoft.com>
2021-09-17 17:15:10 -0400

    Added Screen Capture Protection &FSLogix condition

--- solutions/avd/scripts/Set-SessionHostConfiguration.ps1
+++ solutions/avd/scripts/Set-SessionHostConfiguration.ps1
@@ -24,6 +24,14 @@ Param(
     
     [parameter(Mandatory)]
     [string]
+    $PooledHostPool,
+
+    [parameter(Mandatory)]
+    [string]
+    $ScreenCaptureProtection,
+
+    [parameter(Mandatory)]
+    [string]
     $StorageAccountName
 
 )
@@ -44,74 +52,87 @@ New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\StorageS
 
 
 ###############################
+#  Screen Capture Protection
+###############################
+
+if($ScreenCaptureProtection -eq 'true')
+{
+    # Enable Screen Capture Protection: https://docs.microsoft.com/en-us/azure/virtual-desktop/screen-capture-protection
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name 'fEnableScreenCaptureProtect' -PropertyType 'DWord' -Value 1
+}
+
+
+###############################
 #  FSLogix Configurations
 ###############################
 
-$Suffix = switch($Environment)
+if($PooledHostPool -eq 'true')
 {
-    AzureCloud {'.file.core.windows.net'}
-    AzureUSGovernment {'.file.core.usgovcloudapi.net'}
-}
-$FileShare = '\\' + $StorageAccountName + $Suffix + '\' + $HostPoolName
+    $Suffix = switch($Environment)
+    {
+        AzureCloud {'.file.core.windows.net'}
+        AzureUSGovernment {'.file.core.usgovcloudapi.net'}
+    }
+    $FileShare = '\\' + $StorageAccountName + $Suffix + '\' + $HostPoolName
 
-# Enables FSLogix profile containers
-New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'Enabled' -PropertyType 'DWord' -Value 1
+    # Enables FSLogix profile containers
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'Enabled' -PropertyType 'DWord' -Value 1
 
-# Deletes a local profile if it exists and matches the profile being loaded from VHD
-New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'DeleteLocalProfileWhenVHDShouldApply' -PropertyType 'DWord' -Value 1
+    # Deletes a local profile if it exists and matches the profile being loaded from VHD
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'DeleteLocalProfileWhenVHDShouldApply' -PropertyType 'DWord' -Value 1
 
-# The folder created in the FSLogix fileshare will begin with the username instead of the SID
-New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'FlipFlopProfileDirectoryName' -PropertyType 'DWord' -Value 1
+    # The folder created in the FSLogix fileshare will begin with the username instead of the SID
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'FlipFlopProfileDirectoryName' -PropertyType 'DWord' -Value 1
 
-# Loads FRXShell if there's a failure attaching to, or using an existing profile VHD(X)
-New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'PreventLoginWithFailure' -PropertyType 'DWord' -Value 1
+    # Loads FRXShell if there's a failure attaching to, or using an existing profile VHD(X)
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'PreventLoginWithFailure' -PropertyType 'DWord' -Value 1
 
-# Loads FRXShell if it's determined a temp profile has been created
-New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'PreventLoginWithTempProfile' -PropertyType 'DWord' -Value 1
+    # Loads FRXShell if it's determined a temp profile has been created
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'PreventLoginWithTempProfile' -PropertyType 'DWord' -Value 1
 
-# List of file system locations to search for the user's profile VHD(X) file
-New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'VHDLocations' -PropertyType 'MultiString' -Value $FileShare
+    # List of file system locations to search for the user's profile VHD(X) file
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'VHDLocations' -PropertyType 'MultiString' -Value $FileShare
 
-# Defender Exclusions for FSLogix
-$Files = @(
-    "%ProgramFiles%\FSLogix\Apps\frxdrv.sys",
-    "%ProgramFiles%\FSLogix\Apps\frxdrvvt.sys",
-    "%ProgramFiles%\FSLogix\Apps\frxccd.sys",
-    "%TEMP%\*.VHD",
-    "%TEMP%\*.VHDX",
-    "%Windir%\TEMP\*.VHD",
-    "%Windir%\TEMP\*.VHDX",
-    "$FileShare\*.VHD",
-    "$FileShare\*.VHDX"
-)
-
-$CloudCache = Get-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'CCDLocations' -ErrorAction 'SilentlyContinue'
-if($CloudCache)
-{ 
-    $Files += @(
-        "%ProgramData%\FSLogix\Cache\*.VHD"
-        "%ProgramData%\FSLogix\Cache\*.VHDX"
-        "%ProgramData%\FSLogix\Proxy\*.VHD"
-        "%ProgramData%\FSLogix\Proxy\*.VHDX"
+    # Defender Exclusions for FSLogix
+    $Files = @(
+        "%ProgramFiles%\FSLogix\Apps\frxdrv.sys",
+        "%ProgramFiles%\FSLogix\Apps\frxdrvvt.sys",
+        "%ProgramFiles%\FSLogix\Apps\frxccd.sys",
+        "%TEMP%\*.VHD",
+        "%TEMP%\*.VHDX",
+        "%Windir%\TEMP\*.VHD",
+        "%Windir%\TEMP\*.VHDX",
+        "$FileShare\*.VHD",
+        "$FileShare\*.VHDX"
     )
+
+    $CloudCache = Get-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'CCDLocations' -ErrorAction 'SilentlyContinue'
+    if($CloudCache)
+    { 
+        $Files += @(
+            "%ProgramData%\FSLogix\Cache\*.VHD"
+            "%ProgramData%\FSLogix\Cache\*.VHDX"
+            "%ProgramData%\FSLogix\Proxy\*.VHD"
+            "%ProgramData%\FSLogix\Proxy\*.VHDX"
+        )
+    }
+
+    foreach($File in $Files)
+    {
+        Add-MpPreference -ExclusionPath $File
+    }
+
+    $Processes = @(
+        "%ProgramFiles%\FSLogix\Apps\frxccd.exe",
+        "%ProgramFiles%\FSLogix\Apps\frxccds.exe",
+        "%ProgramFiles%\FSLogix\Apps\frxsvc.exe"
+    )
+
+    foreach($Process in $Processes)
+    {
+        Add-MpPreference -ExclusionProcess $Process
+    }
 }
-
-foreach($File in $Files)
-{
-    Add-MpPreference -ExclusionPath $File
-}
-
-$Processes = @(
-    "%ProgramFiles%\FSLogix\Apps\frxccd.exe",
-    "%ProgramFiles%\FSLogix\Apps\frxccds.exe",
-    "%ProgramFiles%\FSLogix\Apps\frxsvc.exe"
-)
-
-foreach($Process in $Processes)
-{
-    Add-MpPreference -ExclusionProcess $Process
-}
-
 
 ###############################
 #  Windows 10 Optimizations

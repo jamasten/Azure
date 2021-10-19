b853fd2800cfafe039aeae83058c5d5c09132f9c
valerio-battelle <valerio@battelle.org>
2021-10-19 16:25:04 -0400

    finally got AVD to deploy in the test tenant

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@ provider "azurerm" {
 }
 
 resource "azurerm_subscription_template_deployment" "avd" {
-    name = "avdmvdev"
+    name = "avdmvtest"
     location = "usgovvirginia"
     template_content = file("./solutions/avd/solution.json")
     parameters_content = file("./parameters/test-parameters-valerio.json")
+    # parameters_content = file("./parameters/battelleus-parameters-valerio.json")
 }

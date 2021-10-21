d6dda7a50265d5f6a8f1f59f84d97bcdfc23f91d
valerio-battelle <valerio@battelle.org>
2021-10-21 14:57:46 -0400

    refactor from json parameter file to terraform variables.  creates a user that is able to join vm's to the domain instead of having to pass in an existing username and password.  creates an avd_users group specific to the resource name suffix.  see #20

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,20 +1,40 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.terraform.io/hashicorp/azurerm" {
-  version = "2.81.0"
+provider "registry.terraform.io/hashicorp/azuread" {
+  version     = "2.6.0"
+  constraints = "2.6.0"
   hashes = [
-    "h1:VZEhiI0ly+qMcDF9iji9AvOKVkU9Ox5nStKI7agqXeU=",
-    "zh:087f048995b70fb18c8e098d099085dbbb5102d157833405008ef0264860d524",
-    "zh:112f8897a30ff1ed865625a18dc298114ed91ad078c71753604aae2cd23bf832",
-    "zh:1cb29842b3946ba95d5ee76431b09dfa01b64669ef593285fbce2f87cb2d1916",
-    "zh:21b5ae1ddb0781d9e6ce98484e6c6c674c9c4d2267b524db730671f4a0d47c0e",
-    "zh:36d95984e27aa8ed6218be231ba9ef3e94e7ac8a1c452fdb203b278018385251",
-    "zh:5f01f7acf7467d78c984c4318ffefeae421c54bb8affa0243c188c6795922819",
-    "zh:5f3046434fde4255bfd094dc6f926a3b88761d2ce022aaeb4eb78599c945903e",
-    "zh:68d948ed7144c00554ee0f3b0022ebd6a5e670fa9bb60ab51408b8cfebd4f4b4",
-    "zh:69b777b554c849b1624bb4d1bb10a68ab697e04dd2271026dd6782ee6e05b780",
-    "zh:a9283814ee9438aecb48f3a2bb092ca8437af91a5eef3aed4fbfd75ab8fb8268",
-    "zh:f049c61896d1c58b9e20b42692bc41a5014ba45a7ca40e1dd346cc6db99ba6c5",
+    "h1:Zmsa6HlKVe+Kyx/3tlcKHTZemiF2f9ZiVhRATmcb9hk=",
+    "zh:06ab7d8818c3d3d21f760e7208c88fc7cdcdef1b38245434673ff3d453f9adf6",
+    "zh:46090b2491b77db00d7bf4fe87700ff3b3e23a88fbab334ac46b13f14c9f0fc5",
+    "zh:5780305f55e0dabdbaca01f40fa99dc527ac8945f9f667c0d5a307dd079e32cc",
+    "zh:622e6ecd2419679984b8df92f8791365f12ed8795b40d1fdf7732d460aec53ea",
+    "zh:6e5cf7c600a6cdce3a61a6b1e42c3df41a304fae3a46fa5eef6bd690d13c1c91",
+    "zh:78a257a92fdad79bafb63e15b650be95ee23cd256a028c7a0c62dd4317a9c19c",
+    "zh:8336116e5ed91856d64dbcc921437a71a3825ac3f3a31ab246dfcd0ded1a84a8",
+    "zh:9a5a704dd8083a3fe4a70fd95dcc1cfa8e9d86a8cada7b6392b8fb7cf619e495",
+    "zh:ac3401f501aca6e04cb77da1334ebda7696c21f56bc3e1148c856862704ab7f9",
+    "zh:b35b4b588acfe863f061aa4dbe938b50ed719ef423dccfd9673d594394c3b25b",
+    "zh:fe93c7645505166c69627f6d2a86daaf38d829bc02a5553a12826841fc5a27cd",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "2.80.0"
+  constraints = "~> 2.80.0"
+  hashes = [
+    "h1:hdeAgSZUaq54ornF5yquG/c91U+Ou6cMaQTQj3TVLRc=",
+    "zh:08b09b80ff1f759d3cafa57e07bf3c3b50ab8148fbec3df7508825eb64f077b9",
+    "zh:1f7d9d7638c717faf92cb2283f53f586915a3b646f8b09157876d87515795535",
+    "zh:231255fa6c4434b9221623da02ce397fb3e894feeebed62b0181de775ed93769",
+    "zh:2765faa3dc09e99bd3d5c5fc84d62953bda411a4dcd65ddcbc4a37d0aa245e95",
+    "zh:3ef55ae0b81f0cc9bbc17a3992f54868a7368c012ce68dd8ed80db79efd10f14",
+    "zh:6a90b95872f941e93b97086d9a6d2cc7c1a1db4fb8ff54472f22207a350266f2",
+    "zh:7877496115affc512befcef8e555670a93678f8955aba8353c58dc63b851551c",
+    "zh:8556030dcd628350accdefd3ac02980285213e7211195663f9d10793ec3791f6",
+    "zh:8ef7452e4dd67dc5591186e8ec087db8b41272e24fa56929e24cbe029e909b2c",
+    "zh:9b29361f2c6f4798c0c31aac18dc00ddf932d9d431c8bcf359ae96538ea7ba13",
+    "zh:fd9bc393454af6396901806333ff1f60d1806bcda1711b4e768843174c14ef30",
   ]
 }
--- main.tf
+++ main.tf
@@ -1,12 +1,235 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.6.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.80.0"
+    }
+  }
+}
+provider "azuread" {
+  environment = "usgovernment"
+}
+
 provider "azurerm" {
   features {}
   skip_provider_registration = true
+  # delete_nested_items_during_deletion = true
+}
+
+########################################
+# Inputs
+########################################
+
+variable "domain_name" {
+  type = string
+}
+variable "dc_admins_group_object_id" {
+  type = string
+  description = "the object id of the AAD DC Administrators Azure Active Directory group"
+}
+variable "drain_mode" {
+  type = bool
+  default = false
+}
+variable "ephemeral_os_disk" {
+  type = bool
+  default = false
+}
+variable "fs_logix" {
+  type = bool
+  default = true
+}
+variable "new_or_existing" {
+  type = string
+  default = "new"
+}
+variable "ou_path" {
+  type = string
+  default = "OU=AADDC Computers,DC=battelletest,DC=onmicrosoft,DC=us"
+}
+variable "resource_name_suffix" {
+  type = string
+  default = "mvtest"
+}
+# variable "security_principal_id" {
+#   type = string
+#   description = "the object id of the avd_users azure active directory group"
+# }
+variable "subnet" {
+  type = string
+  default = "avd-dev"
+}
+variable "virtual_network" {
+  type = string
+  default = "avd-dev"
+}
+variable "virtual_network_resource_group" {
+  type = string
+  default = "avd-dev"
+}
+variable "session_host_count" {
+  type = number
+  default = 1
+}
+variable "vm_password" {
+  type = string
+  default = "aaAA11223344"
+}
+variable "vm_username" {
+  type = string
+  default = "azadmin"
+}
+variable "wvd_object_id" {
+  type = string
+  description = "the object id of the Windows Virtual Desktop enterprise application for the tenant"
+}
+variable "recovery_services" {
+  type = bool
+  default = false
+}
+variable "image_sku" {
+  type = string
+  default = "20h2-ent-cpc-m365-g2"
+}
+variable "image_offer" {
+  type = string
+  default = "windows-ent-cpc"
+}
+variable "host_pool_type" {
+  type = string
+  default = "Pooled DepthFirst"
+}
+variable "max_session_limit" {
+  type = number
+  default = 1
+}
+variable "screen_capture_protection" {
+  type = bool
+  default = false
+}
+variable "custom_rdp_property" {
+  type = string
+  default = "audiocapturemode:i:1;camerastoredirect:s:*;use multimon:i:0;drivestoredirect:s:;redirectclipboard:i:0;redirectsmartcards:i:1"
+}
+
+locals {
+  user_principal_name = "${var.resource_name_suffix}-temp-admin-user@${var.domain_name}"
+  user_principal_password = "Pa55w0Rd!!1"
+  avd_users_group_name = "avd_users_${var.resource_name_suffix}"
+}
+
+# setup the temp user
+resource "azuread_user" "admin" {
+  user_principal_name = local.user_principal_name
+  display_name        = "DC Administrator"
+  password            = local.user_principal_password
+}
+
+#setup the avd_users_group
+resource "azuread_group" "avd_users" {
+  display_name = local.avd_users_group_name
+  security_enabled = true
+}
+resource "azuread_group_member" "admin" {
+  group_object_id  = "${var.dc_admins_group_object_id}"
+  member_object_id = azuread_user.admin.object_id
 }
 
 resource "azurerm_subscription_template_deployment" "avd" {
-    name = "avdmvtest"
+    name = "avd"
     location = "usgovvirginia"
     template_content = file("./solutions/avd/solution.json")
-    parameters_content = file("./parameters/test-parameters-valerio.json")
+    parameters_content = jsonencode({
+      "DomainName": {
+        "value": "${var.domain_name}"
+      },
+      "DomainServices": {
+        "value": "ActiveDirectory"
+      },
+      "DomainJoinUserPrincipalName": {
+        "value": "${local.user_principal_name}"
+      },
+      "DomainJoinPassword": {
+        "value": "${local.user_principal_password}"
+      },
+      "DrainMode": {
+        "value": var.drain_mode
+      },
+      "EphemeralOsDisk": {
+        "value": var.ephemeral_os_disk
+      },
+      "FSLogix": {
+        "value": var.fs_logix
+      },
+      "newOrExisting": {
+        "value": "${var.new_or_existing}"
+      },
+      "OuPath": {
+        "value": "${var.ou_path}"
+      },
+      "ResourceNameSuffix": {
+        "value": "${var.resource_name_suffix}"
+      },
+      "SecurityPrincipalId": {
+        "value": "${azuread_group.avd_users.object_id}"
+      },
+      "SecurityPrincipalName": {
+        "value": "${local.avd_users_group_name}"
+      },
+      "Subnet": {
+        "value": "${var.subnet}"
+      },
+      "VirtualNetwork": {
+        "value": "${var.virtual_network}"
+      },
+      "VirtualNetworkResourceGroup": {
+        "value": "${var.virtual_network_resource_group}"
+      },
+      "SessionHostCount": {
+        "value": var.session_host_count
+      },
+      "VmPassword": {
+        "value": "${var.vm_password}"
+      },
+      "VmUsername": {
+        "value": "${var.vm_username}"
+      },
+      "WvdObjectId": {
+        "value": "${var.wvd_object_id}"
+      },
+      "Tags": {
+        "value": {
+          "Owner": "Matt Valerio",
+          "Purpose": "POC",
+          "Environment": "Dev"
+        }
+      },
+      "RecoveryServices": {
+        "value":  var.recovery_services
+      },
+      "ImageSku": {
+        "value": "${var.image_sku}"
+      },
+      "ImageOffer": {
+        "value": "${var.image_offer}"
+      },
+      "HostPoolType": {
+        "value": "${var.host_pool_type}"
+      },
+      "MaxSessionLimit": {
+        "value": var.max_session_limit
+      },
+      "ScreenCaptureProtection": {
+        "value": var.screen_capture_protection
+      },
+      "CustomRdpProperty": {
+        "value": "${var.custom_rdp_property}"
+      }
+    })
+    # parameters_content = file("./parameters/test-parameters-valerio.json")
     # parameters_content = file("./parameters/battelleus-parameters-valerio.json")
 }

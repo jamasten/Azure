22eeb1cc591f6897194b9da2d5ba563ec8bdc021
valerio-battelle <valerio@battelle.org>
2021-10-25 10:58:52 -0400

    addes disk_encryption variable, setting owner attribute in tags to the active azure user, adding descriptions to all variables. see #23

--- main.tf
+++ main.tf
@@ -23,8 +23,14 @@ provider "azurerm" {
 # Inputs
 ########################################
 
+variable "disk_encryption" {
+  type = bool
+  default = true
+  description = "Enable BitLocker encrytion on the AVD session hosts and management VM, if applicable."
+}
 variable "domain_name" {
   type = string
+  description = "The name of the domain that provides ADDS to the AVD session hosts and is synchronized with Azure AD"
 }
 variable "dc_admins_group_object_id" {
   type = string
@@ -33,54 +39,61 @@ variable "dc_admins_group_object_id" {
 variable "drain_mode" {
   type = bool
   default = false
+  description = "Enable drain mode on sessions hosts during deployment to prevent users from accessing the session hosts."
 }
 variable "ephemeral_os_disk" {
   type = bool
   default = false
+  description = "Choose whether the session host uses an ephemeral disk for the operating system.  Be sure to select a VM SKU that offers a temporary disk that meets your image requirements. Reference: https://docs.microsoft.com/en-us/azure/virtual-machines/ephemeral-os-disks"
 }
 variable "fs_logix" {
   type = bool
   default = true
+  description = "Enable FSLogix to manage user profiles for the AVD session hosts."
 }
 variable "new_or_existing" {
   type = string
   default = "new"
+  description = "Sets whether this is the first deployment of this solution or is a follow up deployment to add new or additional AVD session hosts."
 }
 variable "ou_path" {
   type = string
   default = "OU=AADDC Computers,DC=battelletest,DC=onmicrosoft,DC=us"
+  description = "The distinguished name for the target Organization Unit in Active Directory Domain Services."
 }
 variable "resource_name_suffix" {
   type = string
-  default = "mvtest"
+  description = "Use letters and numbers only.  This suffix is used in conjunction with the resource type prefixes to name most of the Azure resources in this solution.  The only exception is the Storage Account since the value must globally unique and has stricter character requirements."
 }
-# variable "security_principal_id" {
-#   type = string
-#   description = "the object id of the avd_users azure active directory group"
-# }
 variable "subnet" {
   type = string
   default = "avd-dev"
+  description = "The subnet for the AVD session hosts."
 }
 variable "virtual_network" {
   type = string
   default = "avd-dev"
+  description = "Virtual network for the AVD sessions hosts"
 }
 variable "virtual_network_resource_group" {
   type = string
   default = "avd-dev"
+  description = "Virtual network resource group for the AVD sessions hosts"
 }
 variable "session_host_count" {
   type = number
   default = 1
+  description = "The number of session hosts to deploy in the host pool"
 }
 variable "vm_password" {
   type = string
   default = "aaAA11223344"
+  description = "Local administrator password for the AVD session hosts"
 }
 variable "vm_username" {
   type = string
   default = "azadmin"
+  description = "The Local Administrator Username for the Session Hosts"
 }
 variable "wvd_object_id" {
   type = string
@@ -89,36 +102,49 @@ variable "wvd_object_id" {
 variable "recovery_services" {
   type = bool
   default = false
+  description = "Enable backups to an Azure Recovery Services vault.  For a pooled host pool, this will enable backups on the Azure file share.  For a personal host pool, this will enable backups on the AVD sessions hosts."
 }
 variable "image_sku" {
   type = string
   default = "20h2-ent-cpc-m365-g2"
+  description = "SKU for the virtual machine image"
 }
 variable "image_offer" {
   type = string
   default = "windows-ent-cpc"
+  description = "Offer for the virtual machine image"
 }
 variable "host_pool_type" {
   type = string
   default = "Pooled DepthFirst"
+  description = "These options specify the host pool type and depending on the type, provides the load balancing options and assignment types."
 }
 variable "max_session_limit" {
   type = number
   default = 1
+  description = "The maximum number of sessions per AVD session host."
 }
 variable "screen_capture_protection" {
   type = bool
   default = false
+  description = "Determines whether the Screen Capture Protection feature is enabled.  As of 9/17/21, this is only supported in Azure Cloud. https://docs.microsoft.com/en-us/azure/virtual-desktop/screen-capture-protection"
 }
 variable "custom_rdp_property" {
   type = string
   default = "audiocapturemode:i:1;camerastoredirect:s:*;use multimon:i:0;drivestoredirect:s:;redirectclipboard:i:0;redirectsmartcards:i:1"
+  description = "Input RDP properties to add or remove RDP functionality on the AVD host pool. Settings reference: https://docs.microsoft.com/en-us/windows-server/remote/remote-desktop-services/clients/rdp-files?context=/azure/virtual-desktop/context/context"
 }
 
 locals {
   user_principal_name = "avd-${var.resource_name_suffix}-temp-admin-user@${var.domain_name}"
   user_principal_password = "Pa55w0Rd!!1"
   avd_users_group_name = "avd_users_${var.resource_name_suffix}"
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azuread_user" "current_user" {
+  object_id = data.azurerm_client_config.current.object_id
 }
 
 #setup the avd_users_group
@@ -135,7 +161,7 @@ resource "azuread_user" "admin" {
 }
 
 resource "azuread_group_member" "admin" {
-  group_object_id  = "${var.dc_admins_group_object_id}"
+  group_object_id  = var.dc_admins_group_object_id
   member_object_id = azuread_user.admin.object_id
 }
 
@@ -144,17 +170,20 @@ resource "azurerm_subscription_template_deployment" "avd" {
     location = "usgovvirginia"
     template_content = file("./solutions/avd/solution.json")
     parameters_content = jsonencode({
+      "DiskEncryption": {
+        "value": var.disk_encryption
+      }
       "DomainName": {
-        "value": "${var.domain_name}"
+        "value": var.domain_name
       },
       "DomainServices": {
         "value": "ActiveDirectory"
       },
       "DomainJoinUserPrincipalName": {
-        "value": "${local.user_principal_name}"
+        "value": local.user_principal_name
       },
       "DomainJoinPassword": {
-        "value": "${local.user_principal_password}"
+        "value": local.user_principal_password
       },
       "DrainMode": {
         "value": var.drain_mode
@@ -166,44 +195,44 @@ resource "azurerm_subscription_template_deployment" "avd" {
         "value": var.fs_logix
       },
       "newOrExisting": {
-        "value": "${var.new_or_existing}"
+        "value": var.new_or_existing
       },
       "OuPath": {
-        "value": "${var.ou_path}"
+        "value": var.ou_path
       },
       "ResourceNameSuffix": {
-        "value": "${var.resource_name_suffix}"
+        "value": var.resource_name_suffix
       },
       "SecurityPrincipalId": {
-        "value": "${azuread_group.avd_users.object_id}"
+        "value": azuread_group.avd_users.object_id
       },
       "SecurityPrincipalName": {
-        "value": "${local.avd_users_group_name}"
+        "value": local.avd_users_group_name
       },
       "Subnet": {
-        "value": "${var.subnet}"
+        "value": var.subnet
       },
       "VirtualNetwork": {
-        "value": "${var.virtual_network}"
+        "value": var.virtual_network
       },
       "VirtualNetworkResourceGroup": {
-        "value": "${var.virtual_network_resource_group}"
+        "value": var.virtual_network_resource_group
       },
       "SessionHostCount": {
         "value": var.session_host_count
       },
       "VmPassword": {
-        "value": "${var.vm_password}"
+        "value": var.vm_password
       },
       "VmUsername": {
-        "value": "${var.vm_username}"
+        "value": var.vm_username
       },
       "WvdObjectId": {
-        "value": "${var.wvd_object_id}"
+        "value": var.wvd_object_id
       },
       "Tags": {
         "value": {
-          "Owner": "Matt Valerio",
+          "Owner": "${data.azuread_user.current_user.user_principal_name}",
           "Purpose": "POC",
           "Environment": "Dev"
         }
@@ -212,13 +241,13 @@ resource "azurerm_subscription_template_deployment" "avd" {
         "value":  var.recovery_services
       },
       "ImageSku": {
-        "value": "${var.image_sku}"
+        "value": var.image_sku
       },
       "ImageOffer": {
-        "value": "${var.image_offer}"
+        "value": var.image_offer
       },
       "HostPoolType": {
-        "value": "${var.host_pool_type}"
+        "value": var.host_pool_type
       },
       "MaxSessionLimit": {
         "value": var.max_session_limit
@@ -227,9 +256,7 @@ resource "azurerm_subscription_template_deployment" "avd" {
         "value": var.screen_capture_protection
       },
       "CustomRdpProperty": {
-        "value": "${var.custom_rdp_property}"
+        "value": var.custom_rdp_property
       }
     })
-    # parameters_content = file("./parameters/test-parameters-valerio.json")
-    # parameters_content = file("./parameters/battelleus-parameters-valerio.json")
 }

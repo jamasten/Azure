2cf2483ff22b28ef62bd7beb041dd6d59417bab3
Jason Masten <jamasten@microsoft.com>
2021-10-24 21:52:43 -0400

    removed loop to add one user, prompt for UPN

--- landingZones/azureActiveDirectoryDomainServices/postDeployment.ps1
+++ landingZones/azureActiveDirectoryDomainServices/postDeployment.ps1
@@ -1,3 +1,4 @@
+$UserPrincipalName = Read-Host -Prompt 'Input UPN to add to AAD DC Administrators group'
 $AdminGroupName = 'AAD DC Administrators'
 
 # Create Security Group for Azure ADDS administration in Azure AD
@@ -10,13 +11,10 @@ if($null -eq (Get-AzADGroup -DisplayName $AdminGroupName))
 
 # Add Azure ADDS Admin Account to Security Group in Azure AD
 Start-Sleep 10
-$Users = Get-AzADUser | Where-Object {$_.UserPrincipalName -like "jamasten*" -or $_.UserPrincipalName -like "admin*"}
-foreach($User in $Users)
+$User = Get-AzADUser | Where-Object {$_.UserPrincipalName -eq $UserPrincipalName}
+if($null -eq (Get-AzADGroupMember -GroupDisplayName $AdminGroupName | Where-Object {$_.UserPrincipalName -eq $User.UserPrincipalName}))
 {
-  if($null -eq (Get-AzADGroupMember -GroupDisplayName $AdminGroupName | Where-Object {$_.UserPrincipalName -eq $User.UserPrincipalName}))
-  {
-    Add-AzADGroupMember `
-      -TargetGroupDisplayName $AdminGroupName `
-      -MemberUserPrincipalName $User.UserPrincipalName
-  }
+  Add-AzADGroupMember `
+    -TargetGroupDisplayName $AdminGroupName `
+    -MemberUserPrincipalName $User.UserPrincipalName
 }

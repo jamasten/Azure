48446ee87358a0b94783a3f11bcb21fdf2f31daf
Jason Masten <jamasten@microsoft.com>
2021-10-28 22:50:17 -0400

    added utility scripts

---
+++ utilities/Get-AzureLocationTimezones.ps1
@@ -0,0 +1,46 @@
+﻿# Bing Maps API Key
+$Key = ''
+
+# Log in first with Connect-AzAccount
+$azContext = Get-AzContext
+$azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+$profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+$token = $profileClient.AcquireAccessToken($azContext.Subscription.TenantId)
+$Header = @{
+    'Content-Type'='application/json'
+    'Authorization'='Bearer ' + $token.AccessToken
+}
+$subscriptionId = $azContext.Subscription.Id
+
+$URI = "https://management.azure.com/subscriptions/$subscriptionId/locations?api-version=2020-01-01"
+#$URI = "https://management.usgovcloudapi.net/subscriptions/$subscriptionId/locations?api-version=2020-01-01"
+
+$Locations = (Invoke-RestMethod `
+    -Headers $Header `
+    -Method Get `
+    -Uri $URI `
+    -Verbose).value
+
+$Data = @()
+foreach($Location in $Locations)
+{
+    if($Location -notlike "*(Stage)")
+    {
+        $Uri = "https://dev.virtualearth.net/REST/v1/TimeZone/$($Location.metadata.latitude),$($Location.metadata.longitude)?&key=$Key"
+        if($Location.metadata.latitude)
+        {
+            $TimeZone = (Invoke-RestMethod -Method Get -Uri $Uri).resourceSets.resources.timeZone.abbreviation
+        }
+        else
+        {
+            $TimeZone = ''
+        }
+        $Data += [pscustomobject][ordered]@{
+            Name = $Location.displayName
+            TimeZone = $TimeZone
+        }
+        Start-Sleep 1
+    }
+}
+
+$Data | Sort-Object Name
---
+++ utilities/Get-AzureLocations.ps1
@@ -0,0 +1,40 @@
+﻿# Bing Maps API Key
+$Key = ''
+
+# Log in first with Connect-AzAccount
+$azContext = Get-AzContext
+$azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+$profileClient = New-Object -TypeName Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient -ArgumentList ($azProfile)
+$token = $profileClient.AcquireAccessToken($azContext.Subscription.TenantId)
+$Header = @{
+    'Content-Type'='application/json'
+    'Authorization'='Bearer ' + $token.AccessToken
+}
+$subscriptionId = $azContext.Subscription.Id
+
+$URI = "https://management.azure.com/subscriptions/$subscriptionId/locations?api-version=2020-01-01"
+
+$Locations = (Invoke-RestMethod `
+    -Headers $Header `
+    -Method Get `
+    -Uri $URI `
+    -Verbose).value
+
+$Data = @()
+foreach($Location in $Locations)
+{
+    if($Location -notlike "*(Stage)")
+    {
+        $Uri = "http://dev.virtualearth.net/REST/v1/Locations/$($Location.metadata.latitude),$($Location.metadata.longitude)?key=$Key"
+        $Address = (Invoke-RestMethod -Method Get -Uri $Uri).resourceSets.resources.address
+        $Data += [pscustomobject][ordered]@{
+            Name = $Location.displayName
+            City = $Address.locality
+            State = $Address.adminDistrict
+            Country = $Address.countryRegion
+        }
+        Start-Sleep 1
+    }
+}
+
+$Data

44087370fa1d7656ccb37deb6fbf9319c9f07a84
Jason Masten <jamasten@microsoft.com>
2021-12-17 16:35:18 -0500

    Added script

---
+++ utilities/Set-AzureAcceleratedNetworking.ps1
@@ -0,0 +1,16 @@
+﻿param(
+    [Parameter(Mandatory)]
+    [string]$NicName,
+
+    [Parameter(Mandatory)]
+    [string]$ResourceGroupName,
+
+    [Parameter(Mandatory)]
+    [string]$VirtualMachineName
+)
+
+Stop-AzVM -ResourceGroup $ResourceGroupName -Name $VirtualMachineName
+$nic = Get-AzNetworkInterface -ResourceGroupName $ResourceGroupName -Name $NicName
+$nic.EnableAcceleratedNetworking = $true
+$nic | Set-AzNetworkInterface
+Start-AzVM -ResourceGroup $ResourceGroupName -Name $VirtualMachineName

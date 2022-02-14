629c56264dff0953cfd4f370e4a796792fc47d9f
Jason Masten <jamasten@microsoft.com>
2022-02-13 22:56:28 -0500

    added utility script for AVD notifications

---
+++ utilities/Send-AzureVirtualDesktopNotifications.ps1
@@ -0,0 +1,18 @@
+﻿$HostPoolName = ''
+$ResourceGroupName = ''
+$Time = '18:00EST'
+
+$Sessions = Get-AzWvdUserSession -HostPoolName $HostPoolName -ResourceGroupName $ResourceGroupName
+foreach($Session in $Sessions)
+{
+    $SessionHostName = $Session.Id.split('/')[-3]
+    $UserSessionId = $Session.Id.split('/')[-1]
+
+    Send-AzWvdUserSessionMessage  `
+        -ResourceGroupName $ResourceGroupName `
+        -HostPoolName $HostPoolName `
+        -SessionHostName $SessionHostName `
+        -UserSessionId $UserSessionId `
+        -MessageBody "Maintenance will begin in 1 hour at $Time" `
+        -MessageTitle 'Upcoming Maintenance'
+}

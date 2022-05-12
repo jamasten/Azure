3c83ad31d09373bc5907b0e6d9322faa6b8b774d
Jason Masten <jamasten@microsoft.com>
2022-05-12 11:36:07 -0400

    Added license & help

--- utilities/Export-AzureComputeGalleryImageVersion.ps1
+++ utilities/Export-AzureComputeGalleryImageVersion.ps1
@@ -1,5 +1,51 @@
-# REFERENCE: https://docs.microsoft.com/en-us/azure/virtual-machines/managed-disk-from-image-version
+<#
+MIT License
 
+Copyright (c) 2022 Jason Masten
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+.SYNOPSIS
+Exports an Image Version from an Azure Compute Gallery.
+.DESCRIPTION
+Exports a VHD of a Managed Disk created from an Image Version in an Azure Compute Gallery.  The VHD is saved to the Downloads folder in the user's profile that is running the script.
+.PARAMETER ComputeGalleryDefinitionName
+The name of the Image Definition in the Azure Compute Gallery.
+.PARAMETER ComputeGalleryName
+The name of the Azure Compute Gallery.
+.PARAMETER ComputeGalleryResourceGroupName
+The name of the Resource Group that contains the Azure Compute Gallery.
+.PARAMETER ComputeGalleryVersion
+The name of the Image Version in the Azure Compute Gallery.
+.NOTES
+  Version:        1.0
+  Author:         Jason Masten
+  Creation Date:  2022-05-11
+.EXAMPLE
+.\Export-AzureComputeGalleryImageVersion.ps1 `
+    -ComputeGalleryDefinitionName 'WindowsServer2019Datacenter' `
+    -ComputeGalleryName 'cg_shared_d_va' `
+    -ComputeGalleryResourceGroupName 'rg-images-d-va' `
+    -ComputeGalleryVersion '1.0.0'
+
+This example creates a Managed Disk from an Image Version in an Azure Compute Gallery. Downloads the VHD of the Managed Disk to the user's Downloads folder and validates the auto-generated MD5 hash.  Once the download completes, the Managed Disk is deleted. 
+#>
 [Cmdletbinding()]
 param(
 

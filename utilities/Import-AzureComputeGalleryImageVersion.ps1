4e39c9e72f58c76529f346ed825da1b4742953f4
Jason Masten <jamasten@microsoft.com>
2022-05-12 11:07:31 -0400

    Add license & help, Updated comments & params

--- utilities/Import-AzureComputeGalleryImageVersion.ps1
+++ utilities/Import-AzureComputeGalleryImageVersion.ps1
@@ -1,3 +1,75 @@
+<#
+MIT License
+
+Copyright (c) 2022 Jason Masten
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+.SYNOPSIS
+Exports an Image Version from an Azure Compute Gallery.
+.DESCRIPTION
+Exports an Image Version from an Azure Compute Gallery.
+.PARAMETER ComputeGalleryDefinitionName
+The name of the Image Definition in the Azure Compute Gallery.
+.PARAMETER ComputeGalleryName
+The name of the Azure Compute Gallery.
+.PARAMETER ComputeGalleryResourceGroupName
+The name of the Resource Group that contains the Azure Compute Gallery.
+.PARAMETER ComputeGalleryVersion
+The name of the Image Version in the Azure Compute Gallery.
+.PARAMETER ImageOffer
+The offer of the image to create a new Image Defintion if needed.
+.PARAMETER ImagePublisher
+The publisher of the image to create a new Image Defintion if needed.
+.PARAMETER ImageSku
+The SKU of the image to create a new Image Defintion if needed.
+.PARAMETER ImageState
+The state of the image to create a new Image Defintion if needed.
+.PARAMETER VhdFilePath
+The file path to the VHD on the filesystem.
+.NOTES
+  Version:        1.0
+  Author:         Jason Masten
+  Creation Date:  2022-05-11
+.EXAMPLE
+.\Import-AzureComputeGalleryImageVersion.ps1 `
+    -ComputeGalleryDefinitionName 'WindowsServer2019Datacenter' `
+    -ComputeGalleryName 'cg_shared_d_eu' `
+    -ComputeGalleryResourceGroupName 'rg-images-d-eu' `
+    -ComputeGalleryVersion '1.0.0' `
+    -ImageOffer 'WindowsServer' `
+    -ImagePublisher 'MicrosoftWindowsServer' `
+    -ImageSku '2019-datacenter' `
+    -ImageState 'generalized' `
+    -VhdFilePath $HOME\Downloads\disk-WindowsServer2019Datacenter.vhd
+
+This example imports the VHD as a Managed Disk, creates a new Image Defintion, and imports the Managed Disk as a new Image Version into the new Image Defintion.
+.EXAMPLE
+.\Import-AzureComputeGalleryImageVersion.ps1 `
+    -ComputeGalleryDefinitionName 'WindowsServer2019Datacenter' `
+    -ComputeGalleryName 'cg_shared_d_eu' `
+    -ComputeGalleryResourceGroupName 'rg-images-d-eu' `
+    -ComputeGalleryVersion '1.0.0' `
+    -VhdFilePath $HOME\Downloads\disk-WindowsServer2019Datacenter.vhd
+
+This example imports the VHD as a Managed Disk and imports the Managed Disk as a new Image Version into an existing Image Defintion.
+#>
 [Cmdletbinding()]
 param(
 
@@ -13,16 +85,16 @@ param(
     [parameter(Mandatory)]
     [string]$ComputeGalleryVersion,
 
-    [parameter(Mandatory)]
+    [parameter(Mandatory=$false)]
     [string]$ImageOffer,
 
-    [parameter(Mandatory)]
+    [parameter(Mandatory=$false)]
     [string]$ImagePublisher,
 
-    [parameter(Mandatory)]
+    [parameter(Mandatory=$false)]
     [string]$ImageSku,
 
-    [parameter(Mandatory)]
+    [parameter(Mandatory=$false)]
     [ValidateSet('generalized','specialized')]
     [string]$ImageState,
 
@@ -36,7 +108,7 @@ $ErrorActionPreference = 'Stop'
 $DiskName = "disk-$($ComputeGalleryDefinitionName)"
 $Location = (Get-AzResourceGroup -Name $ComputeGalleryResourceGroupName).Location
 
-# Uploads VHD from filesystem to Azure as a Managed Disk
+# Uploads the VHD from the filesystem to Azure as a Managed Disk
 Add-AzVhd `
     -LocalFilePath $VhdFilePath `
     -ResourceGroupName $ComputeGalleryResourceGroupName `
@@ -44,18 +116,18 @@ Add-AzVhd `
     -DiskName $DiskName `
     -NumberOfUploaderThreads 32
 
-# Get Disk information
+# Gets the information for the Managed Disk
 $Disk = Get-AzDisk `
     -ResourceGroupName $ComputeGalleryResourceGroupName `
     -DiskName $DiskName
 
-# Check if Compute Gallery exists
+# Checks if the Compute Gallery exists
 $Gallery = Get-AzGallery `
     -ResourceGroupName $ComputeGalleryResourceGroupName `
     -Name $ComputeGalleryName `
     -ErrorAction 'SilentlyContinue'
 
-# If Compute Gallery doesn't exist, create it
+# If the Compute Gallery doesn't exist, create it
 if(!$Gallery)
 { 
     $Gallery = New-AzGallery `
@@ -64,14 +136,14 @@ if(!$Gallery)
         -Location $Location
 }
 
-# Check if Image Definition exists
+# Checks if the Image Definition exists
 $ImageDefinition = Get-AzGalleryImageDefinition `
     -GalleryName $ComputeGalleryName `
     -ResourceGroupName $ComputeGalleryResourceGroupName `
     -Name $ComputeGalleryDefinitionName `
     -ErrorAction 'SilentlyContinue'
 
-# If Image Definition doesn't exist, create it
+# If the Image Definition doesn't exist, create it
 if(!$ImageDefinition)
 { 
     $ImageDefinition = New-AzGalleryImageDefinition `
@@ -86,10 +158,10 @@ if(!$ImageDefinition)
         -Sku $ImageSku
 }
 
-# Get source disk
+# Creates the Source object used to create the Image Version
 $OsDiskImage = @{Source = @{Id = $Disk.Id }}
 
-# Create new gallery image version using the managed disk
+# Create a new Image Version using the Managed Disk
 New-AzGalleryImageVersion `
     -GalleryImageDefinitionName $ComputeGalleryDefinitionName `
     -GalleryImageVersionName '1.0.0' `
@@ -98,7 +170,7 @@ New-AzGalleryImageVersion `
     -Location $Location `
     -OSDiskImage $OsDiskImage
 
-# Delete the managed disk
+# Deletes the Managed Disk
 Remove-AzDisk `
     -ResourceGroupName $Disk.ResourceGroupName `
     -DiskName $Disk.Name `

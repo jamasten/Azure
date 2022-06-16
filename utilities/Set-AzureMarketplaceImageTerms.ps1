3d642565f1eedeed6f43388ca8ba742405cd13db
Jason Masten <jamasten@microsoft.com>
2022-06-15 21:49:55 -0400

    Added script

---
+++ utilities/Set-AzureMarketplaceImageTerms.ps1
@@ -0,0 +1,11 @@
+﻿param(
+    $ImagePublisher,
+    $ImageOffer,
+    $ImageSku
+)
+
+$Terms = Get-AzMarketplaceTerms -Publisher $ImagePublisher -Product $ImageOffer -Name $ImageSku
+if(!($Terms).Accepted)
+{
+    Set-AzMarketplaceTerms -InputObject $Terms -Reject | Out-Null
+}

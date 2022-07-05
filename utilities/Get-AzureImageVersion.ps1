d9b973167f42ce03e587afea891439d1d9c5d30b
Jason Masten <jamasten@microsoft.com>
2022-07-05 10:09:58 -0400

    Added Win 11

--- utilities/Get-AzureImageVersion.ps1
+++ utilities/Get-AzureImageVersion.ps1
@@ -6,7 +6,8 @@ $offer = 'office-365'
 #$offer = 'windows-evd'
 #$offer = 'Windows-10'
 #$sku = '2019-Datacenter-Core'
-$sku = '21h1-evd-o365pp'
+#$sku = '21h1-evd-o365pp'
+$sku = 'win11-21h2-avd-m365'
 (Get-AzVMImagePublisher -Location $location).PublisherName
 (Get-AzVMImageOffer -Location $location -PublisherName $publisher).Offer
 (Get-AzVMImageSku -Location $location -PublisherName $publisher -Offer $offer).Skus

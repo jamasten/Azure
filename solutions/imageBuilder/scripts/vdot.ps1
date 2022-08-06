72bc057d432a202c12bf80cb2a563a959ffd568a
Jason Masten <jamasten@microsoft.com>
2022-08-05 20:50:51 -0400

    Replace teams installer with VDOT

---
+++ solutions/imageBuilder/scripts/vdot.ps1
@@ -0,0 +1,48 @@
+##############################################################
+#  Virtual Desktop Optimization Tool (VDOT)
+##############################################################
+# https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool
+try
+{
+    # Download VDOT
+    $URL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/heads/main.zip'
+    $ZIP = 'VDOT.zip'
+    Invoke-WebRequest -Uri $URL -OutFile $ZIP
+    Write-Host 'Downloaded VDOT'
+    
+    # Extract VDOT from ZIP archive
+    Expand-Archive -LiteralPath $ZIP -Force
+    Write-Host 'Extracted VDOT'
+    
+    # Fix to disable AppX Packages
+    # As of 2/8/22, all AppX Packages are enabled by default
+    $Files = (Get-ChildItem -Path .\VDOT\Virtual-Desktop-Optimization-Tool-main -File -Recurse -Filter "AppxPackages.json").FullName
+    foreach($File in $Files)
+    {
+        $Content = Get-Content -Path $File
+        $Settings = $Content | ConvertFrom-Json
+        $NewSettings = @()
+        foreach($Setting in $Settings)
+        {
+            $NewSettings += [pscustomobject][ordered]@{
+                AppxPackage = $Setting.AppxPackage
+                VDIState = 'Disabled'
+                URL = $Setting.URL
+                Description = $Setting.Description
+            }
+        }
+
+        $JSON = $NewSettings | ConvertTo-Json
+        $JSON | Out-File -FilePath $File -Force
+    }
+    Write-Host 'Set VDOT AppxPackage configuration files to "Disabled"'
+
+    # Run VDOT
+    & .\VDOT\Virtual-Desktop-Optimization-Tool-main\Windows_VDOT.ps1 -AcceptEULA
+    Write-Host 'Optimized the operating system using VDOT'
+}
+catch 
+{
+    Write-Host $_
+    throw
+}

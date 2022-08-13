6145d6211d6f0785051b5fb397334543d187e133
Jason Masten <jamasten@microsoft.com>
2022-08-12 20:37:24 -0400

    Reverted the script to an anonymous download

--- solutions/imageBuilder/scripts/projectVisio.ps1
+++ solutions/imageBuilder/scripts/projectVisio.ps1
@@ -2,9 +2,7 @@ $ErrorActionPreference = 'Stop'
 
 try 
 {
-    Connect-AzAccount -Environment 'AzureCloud' -Identity
-    Get-AzStorageBlobContent -AbsoluteUri 'https://stshdsvcdeu000.blob.core.windows.net/artifacts/office.zip' -Destination 'office.zip'
-    #Invoke-WebRequest -Uri 'https://stshdsvcdeu000.blob.core.windows.net/artifacts/office.zip' -OutFile 'Office.zip'
+    Invoke-WebRequest -Uri 'https://stshdsvcdeu000.blob.core.windows.net/artifacts/office.zip' -OutFile 'Office.zip'
     Expand-Archive -LiteralPath '.\office.zip' -Force
     Start-Process -FilePath '.\office\setup.exe' -ArgumentList "/configure .\office\configuration-Office365-x64.xml" -Wait -PassThru
 }

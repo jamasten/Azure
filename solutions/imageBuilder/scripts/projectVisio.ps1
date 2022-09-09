99dcb348190436e0790e57be676bd2378b7c3c60
Jason Masten <jamasten@microsoft.com>
2022-09-09 00:21:35 -0400

    Added output for successful install

--- solutions/imageBuilder/scripts/projectVisio.ps1
+++ solutions/imageBuilder/scripts/projectVisio.ps1
@@ -4,6 +4,7 @@ try
 {
     Expand-Archive -LiteralPath 'C:\temp\office.zip' -Force
     Start-Process -FilePath 'C:\temp\office\setup.exe' -ArgumentList "/configure C:\temp\office\configuration-Office365-x64.xml" -Wait -PassThru
+    Write-Host 'Installed Microsoft Project & Visio'
 }
 catch 
 {

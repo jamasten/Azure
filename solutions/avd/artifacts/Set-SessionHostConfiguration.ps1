a0018eae22163fc607d2878af8d6a877bd8beff0
Jason Masten <jamasten@microsoft.com>
2022-09-09 00:05:53 -0400

    Updated VDOT command to support feature updates

--- solutions/avd/artifacts/Set-SessionHostConfiguration.ps1
+++ solutions/avd/artifacts/Set-SessionHostConfiguration.ps1
@@ -192,7 +192,9 @@ try
         }
 
         # Run VDOT
-        & .\VDOT\Virtual-Desktop-Optimization-Tool-main\Windows_VDOT.ps1 -AcceptEULA
+        & .\VDOT\Virtual-Desktop-Optimization-Tool-main\Windows_VDOT.ps1 -Optimizations 'All' -AdvancedOptimizations 'Edge,RemoveLegacyIE' -AcceptEULA
+
+
         Write-Log -Message 'Optimized the operating system using VDOT' -Type 'INFO'
     }    
 

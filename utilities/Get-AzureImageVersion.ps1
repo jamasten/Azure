2798c750bf98b70d4cf0e5bb2862263c196be508
Jason Masten <jamasten@microsoft.com>
2022-10-19 16:50:07 -0400

    Update vars & formatting

--- utilities/Get-AzureImageVersion.ps1
+++ utilities/Get-AzureImageVersion.ps1
@@ -1,14 +1,22 @@
 ﻿$location = 'eastus'
+#$location = 'usgovvirginia'
+
 $publisher = 'MicrosoftWindowsDesktop'
 #$publisher = 'MicrosoftWindowsServer'
-#$offer = 'WindowsServer'
+
 $offer = 'office-365'
-#$offer = 'windows-evd'
+#$offer = 'Windows-11'
 #$offer = 'Windows-10'
-#$sku = '2019-Datacenter-Core'
-#$sku = '21h1-evd-o365pp'
+#$offer = 'WindowsServer'
+
 $sku = 'win11-21h2-avd-m365'
+#$sku = '21h1-evd-o365pp'
+#$sku = '2019-Datacenter-Core'
+
 (Get-AzVMImagePublisher -Location $location).PublisherName
+
 (Get-AzVMImageOffer -Location $location -PublisherName $publisher).Offer
+
 (Get-AzVMImageSku -Location $location -PublisherName $publisher -Offer $offer).Skus
+
 Get-AzVMImage -Location $location -PublisherName $publisher -Offer $offer -Skus $sku | Select-Object * | Format-List

ced8624ff6d4ca8b7fe0732bb67c73e11ce83476
Jason Masten <jamasten@microsoft.com>
2022-12-22 01:25:23 -0500

    Disabled Network Adapter Buffer Size configuration

--- solutions/imageBuilder/scripts/vdot.ps1
+++ solutions/imageBuilder/scripts/vdot.ps1
@@ -48,6 +48,13 @@ try
     }
     Write-Host 'Disabled all Appx Packages in the configuration files for the Virtual Desktop Optimization Tool'
 
+    # Disable the "Configuring Network Adapter Buffer Size" configuration
+    $Path = 'C:\temp\Virtual-Desktop-Optimization-Tool-main\Windows_VDOT.ps1'
+    $Script = Get-Content -Path $Path
+    $ScriptUpdate = $Script -replace 'Set-NetAdapterAdvancedProperty', '#Set-NetAdapterAdvancedProperty'
+    $ScriptUpdate | Set-Content -Path $Path
+    Write-Host 'Disabled the "Configuring Network Adapter Buffer Size" configuration'
+
     # Run VDOT
     & C:\temp\Virtual-Desktop-Optimization-Tool-main\Windows_VDOT.ps1 -Optimizations 'All' -AdvancedOptimizations 'Edge','RemoveLegacyIE' -AcceptEULA
     Write-Host 'Optimized the operating system using the Virtual Desktop Optimization Tool'

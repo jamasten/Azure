299e7efab6bac86373e09a8526b0527e5e2dcd42
Jason Masten <jamasten@microsoft.com>
2023-02-16 23:27:59 -0500

    Added comments, Refactored code

--- utilities/Set-AzureFilesKerberosAuthentication.ps1
+++ utilities/Set-AzureFilesKerberosAuthentication.ps1
@@ -1,13 +1,66 @@
+<#
+MIT License
+
+Copyright (c) 2022 Jason Masten
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+.SYNOPSIS
+Domain joins an Azure Storage Account
+.DESCRIPTION
+This script will add a storage account to Active Directory Domain Services as a computer object to support kerberos authentication.
+.PARAMETER Environment
+The name of the Azure environment.
+.PARAMETER KerberosEncryptionType
+The type of kerberos encrpytion used on the session hosts, the storage account, and domain controllers.
+.PARAMETER OuPath
+The distinguished name of the organizational unit in Active Directoy Domain Services.
+.PARAMETER StorageAccountName
+The name of the Azure Storage Account.
+.PARAMETER StorageAccountResourceGroupName
+The Resource Group name of the Azure Storage Account.
+.PARAMETER SubscriptionId
+The ID of the Azure Subscription.
+.PARAMETER TenantId
+The ID of the Azure Active Directory tenant.
+.NOTES
+  Version:              1.0
+  Author:               Jason Masten
+  Creation Date:        2023-02-16
+  Last Modified Date:   2023-02-16
+.EXAMPLE
+.\Set-AzureFilesKerberosAuthentication.ps1 `
+    -Environment 'AzureCloud' `
+    -KerberosEncryptionType 'RC4' `
+    -OuPath 'OU=AVD,DC=Fabrikam,DC=COM' `
+    -StorageAccountName 'saavdpeus' `
+    -StorageAccountResourceGroupName 'rg-avd-p-eus' `
+    -SubscriptionId '00000000-0000-0000-0000-000000000000' `
+    -TenantId '00000000-0000-0000-0000-000000000000'
+
+This example domain joins an Azure Storage Account to the AVD organizational unit in the Fabrikam.com domain.
+#>
+
 #Requires -Modules Az.Storage, Az.Accounts, ActiveDirectory
 [Cmdletbinding()]
 param 
 (
-    [Parameter(Mandatory)]
-    [SecureString]$DomainJoinPassword,
-
-    [Parameter(Mandatory=$false)]
-    [String]$DomainJoinUserPrincipalName,
-
     [Parameter(Mandatory=$false)]
     [ValidateSet("AzureCloud","AzureUSGovernment")]
     [String]$Environment = 'AzureCloud',
@@ -16,26 +69,19 @@ param
     [ValidateSet("AES256","RC4")]
     [String]$KerberosEncryptionType = 'RC4',
 
-    [Parameter(Mandatory=$false)]
-    [String]$Netbios,
-
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory)]
     [String]$OuPath,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory)]
     [String]$StorageAccountName,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory)]
     [String]$StorageAccountResourceGroupName,
 
-    [Parameter(Mandatory=$false)]
-    [ValidateSet("core.windows.net","core.usgovcloudapi.net")]
-    [String]$StorageSuffix = 'core.windows.net',
-
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory)]
     [String]$SubscriptionId,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory)]
     [String]$TenantId
 )
 
@@ -46,43 +92,43 @@ $ErrorActionPreference = 'Stop'
 ##############################################################
 #  Variables
 ##############################################################
-# Create Domain credential
-[pscredential]$DomainCredential = New-Object System.Management.Automation.PSCredential ($DomainJoinUserPrincipalName, $DomainJoinPassword)
+# Set Azure storage suffix
+$StorageSuffix = switch($Environment)
+{
+    "AzureCloud"        {"core.windows.net"}
+    "AzureUSGovernment" {"core.usgovcloudapi.net"}
+}
 
 # Get Domain information
-$Domain = Get-ADDomain -Credential $DomainCredential -Current 'LocalComputer'
+$Domain = Get-ADDomain `
+    -Current 'LocalComputer'
+
+# Create suffix for storage account FQDN
 $FilesSuffix = '.file.' + $StorageSuffix    
 
 
 ##############################################################
 #  Process Storage Resources
 ##############################################################
-# Connects to Azure using a User Assigned Managed Identity
-Connect-AzAccount -Environment $Environment -Tenant $TenantId -Subscription $SubscriptionId
+# Connect to Azure
+Connect-AzAccount `
+    -Environment $Environment `
+    -Tenant $TenantId `
+    -Subscription $SubscriptionId
 
-# Get the storage account key
-$StorageKey = (Get-AzStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName)[0].Value
-
-# Create credential for accessing the storage account
-$StorageUsername = 'Azure\' + $StorageAccountName
-$StoragePassword = ConvertTo-SecureString -String "$($StorageKey)" -AsPlainText -Force
-[pscredential]$StorageKeyCredential = New-Object System.Management.Automation.PSCredential ($StorageUsername, $StoragePassword)
-$Credential = $StorageKeyCredential
-
-# Get / create kerberos key for Azure Storage Account
-$KerberosKey = (Get-AzStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName -ListKerbKey | Where-Object {$_.Keyname -contains 'kerb1'}).Value
-if(!$KerberosKey)
-{
-    New-AzStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName -KeyName kerb1
-    $Key = (Get-AzStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName -ListKerbKey | Where-Object {$_.Keyname -contains 'kerb1'}).Value
-} 
-else 
-{
-    $Key = $KerberosKey
-}
+# Create the Kerberos key for the Azure Storage Account
+$Key = (New-AzStorageAccountKey `
+    -ResourceGroupName $StorageAccountResourceGroupName `
+    -Name $StorageAccountName `
+    -KeyName 'kerb1' `
+    | Select-Object -ExpandProperty 'Keys' `
+    | Where-Object {$_.Keyname -eq 'kerb1'}).Value
 
 # Creates a password for the Azure Storage Account in AD using the Kerberos key
-$ComputerPassword = ConvertTo-SecureString -String $Key.Replace("'","") -AsPlainText -Force
+$ComputerPassword = ConvertTo-SecureString `
+    -String $Key `
+    -AsPlainText `
+    -Force
 
 # Create the SPN value for the Azure Storage Account; attribute for computer object in AD 
 $SPN = 'cifs/' + $StorageAccountName + $FilesSuffix
@@ -90,13 +136,29 @@ $SPN = 'cifs/' + $StorageAccountName + $FilesSuffix
 # Create the Description value for the Azure Storage Account; attribute for computer object in AD 
 $Description = "Computer account object for Azure storage account $($StorageAccountName)."
 
-# Create the AD computer object for the Azure Storage Account
-$Computer = Get-ADComputer -Credential $DomainCredential -Filter {Name -eq $StorageAccountName}
+# Check for existing AD computer object for the Azure Storage Account
+$Computer = Get-ADComputer `
+    -Filter {Name -eq $StorageAccountName}
+
+# Remove existing AD computer object for the Azure Storage Account
 if($Computer)
 {
-    Remove-ADComputer -Credential $DomainCredential -Identity $StorageAccountName -Confirm:$false
+    Remove-ADComputer `
+        -Identity $StorageAccountName `
+        -Confirm:$false
 }
-$ComputerObject = New-ADComputer -Credential $DomainCredential -Name $StorageAccountName -Path $OuPath -ServicePrincipalNames $SPN -AccountPassword $ComputerPassword -KerberosEncryptionType $KerberosEncryptionType -Description $Description -PassThru
+
+# Create AD computer object for the Azure Storage Account
+$ComputerObject = New-ADComputer `
+    -Name $StorageAccountName `
+    -Path $OuPath `
+    -ServicePrincipalNames $SPN `
+    -AccountPassword $ComputerPassword `
+    -KerberosEncryptionType $KerberosEncryptionType `
+    -Description $Description `
+    -AllowReversiblePasswordEncryption $false `
+    -Enabled $true `
+    -PassThru
 
 # Update the Azure Storage Account with the domain join 'INFO'
 Set-AzStorageAccount `
@@ -107,24 +169,36 @@ Set-AzStorageAccount `
     -ActiveDirectoryNetBiosDomainName $Domain.NetBIOSName `
     -ActiveDirectoryForestName $Domain.Forest `
     -ActiveDirectoryDomainGuid $Domain.ObjectGUID `
-    -ActiveDirectoryDomainsid $Domain.DomainSID `
+    -ActiveDirectoryDomainSid $Domain.DomainSID `
     -ActiveDirectoryAzureStorageSid $ComputerObject.SID.Value `
     -ActiveDirectorySamAccountName $ComputerObject.SamAccountName `
     -ActiveDirectoryAccountType 'Computer'
-            
+
 # Enable AES256 encryption if selected
 if($KerberosEncryptionType -eq 'AES256')
 {
     # Set the Kerberos encryption on the computer object
-    $DistinguishedName = 'CN=' + $StorageAccountName + ',' + $OuPath
-    Set-ADComputer -Credential $Credential -Identity $DistinguishedName -KerberosEncryptionType 'AES256'
+    Set-ADComputer `
+        -Identity $ComputerObject.DistinguishedName `
+        -KerberosEncryptionType 'AES256'
     
     # Reset the Kerberos key on the Storage Account
-    New-AzStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName -KeyName kerb1
-    $Key = (Get-AzStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName -ListKerbKey | Where-Object {$_.Keyname -contains 'kerb1'}).Value
+    $Key = (New-AzStorageAccountKey `
+        -ResourceGroupName $StorageAccountResourceGroupName `
+        -Name $StorageAccountName `
+        -KeyName 'kerb2' `
+        | Select-Object -ExpandProperty 'Keys' `
+        | Where-Object {$_.Keyname -eq 'kerb2'}).Value
 
-    # Update the password on the computer object with the new Kerberos key on the Storage Account
-    $NewPassword = ConvertTo-SecureString -String $Key -AsPlainText -Force
-    Set-ADAccountPassword -Credential $Credential -Identity $DistinguishedName -Reset -NewPassword $NewPassword
+    # Capture the Kerberos key as a secure string
+    $NewPassword = ConvertTo-SecureString `
+        -String $Key `
+        -AsPlainText `
+        -Force
+
+    # Update the password on the computer object with the new Kerberos key from the Storage Account
+    Set-ADAccountPassword `
+        -Identity $ComputerObject.DistinguishedName `
+        -Reset `
+        -NewPassword $NewPassword
 }
-

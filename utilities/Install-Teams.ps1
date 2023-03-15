717b1dc7d1ab250082db1232db4855291f003480
Jason Masten <jamasten@microsoft.com>
2023-03-14 21:31:26 -0400

    Added functions; Refactored code

--- utilities/Install-Teams.ps1
+++ utilities/Install-Teams.ps1
@@ -1,45 +1,94 @@
-[Cmdletbinding()]
-Param(
+# Install Teams in per-machine mode for Windows multi-session operating systems
+# This script was developed to install Teams using Azure Image Builder
 
-    [parameter(Mandatory)]
-    [string]
-    $ImageSku
-
-)
- 
-# Set Registry Key & Setting for AVD Media Optimization
-New-Item -Path 'HKLM:\SOFTWARE\Microsoft' -Name 'Teams'
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Teams' -Name 'IsWVDEnvironment' -PropertyType 'Dword' -Value 1
-
-# Install Visual C++
-$appName = 'teams'
-$drive = 'C:\'
-New-Item -Path $drive -Name $appName  -ItemType 'Directory' -ErrorAction 'SilentlyContinue'
-$LocalPath = $drive + '\' + $appName 
-set-Location $LocalPath
-$visCplusURL = 'https://aka.ms/vs/16/release/vc_redist.x64.exe'
-$visCplusURLexe = 'vc_redist.x64.exe'
-$outputPath = $LocalPath + '\' + $visCplusURLexe
-Invoke-WebRequest -Uri $visCplusURL -OutFile $outputPath
-Start-Process -FilePath $outputPath -Args "/install /quiet /norestart /log vcdist.log" -Wait
-
-# Install Teams WebSocket Service
-$webSocketsURL = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4AQBt'
-$webSocketsInstallerMsi = 'webSocketSvc.msi'
-$outputPath = $LocalPath + '\' + $webSocketsInstallerMsi
-Invoke-WebRequest -Uri $webSocketsURL -OutFile $outputPath
-Start-Process -FilePath msiexec.exe -Args "/I $outputPath /quiet /norestart /log webSocket.log" -Wait
-
-# Install Teams
-$teamsURL = 'https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true'
-$teamsMsi = 'teams.msi'
-$outputPath = $LocalPath + '\' + $teamsMsi
-Invoke-WebRequest -Uri $teamsURL -OutFile $outputPath
-if($ImageSku -like "*evd*")
+function Write-Log
 {
-    Start-Process -FilePath msiexec.exe -Args "/I $outputPath /quiet /norestart /log teams.log ALLUSER=1 ALLUSERS=1" -Wait
+    param(
+        [parameter(Mandatory)]
+        [string]$Message,
+        
+        [parameter(Mandatory)]
+        [string]$Type
+    )
+    $Timestamp = Get-Date -Format 'MM/dd/yyyy HH:mm:ss.ff'
+    $Entry = '[' + $Timestamp + '] [' + $Type + '] ' + $Message
+    Write-Host $Entry
 }
-else
+
+function Set-RegistrySetting 
 {
-    Start-Process -FilePath msiexec.exe -Args "/I $outputPath /quiet /norestart /log teams.log" -Wait
+    Param(
+        [parameter(Mandatory=$false)]
+        [String]$Name,
+
+        [parameter(Mandatory=$false)]
+        [String]$Path,
+
+        [parameter(Mandatory=$false)]
+        [String]$PropertyType,
+
+        [parameter(Mandatory=$false)]
+        [String]$Value
+    )
+
+    # Create registry key(s) if necessary
+    if(!(Test-Path -Path $Path))
+    {
+        New-Item -Path $Path -Force
+    }
+
+    # Checks for existing registry setting
+    $Value = Get-ItemProperty -Path $Path -Name $Name -ErrorAction 'SilentlyContinue'
+    $LogOutputValue = 'Path: ' + $Path + ', Name: ' + $Name + ', PropertyType: ' + $PropertyType + ', Value: ' + $Value
+    
+    # Creates the registry setting when it does not exist
+    if(!$Value)
+    {
+        New-ItemProperty -Path $Path -Name $Name -PropertyType $PropertyType -Value $Value -Force
+        Write-Log -Message "Added registry setting: $LogOutputValue" -Type 'INFO'
+    }
+    # Updates the registry setting when it already exists
+    elseif($Value.$($Name) -ne $Value)
+    {
+        Set-ItemProperty -Path $Path -Name $Name -Value $Value -Force
+        Write-Log -Message "Updated registry setting: $LogOutputValue" -Type 'INFO'
+    }
+    # Writes log output when registry setting has the correct value
+    else 
+    {
+        Write-Log -Message "Registry setting exists with correct value: $LogOutputValue" -Type 'INFO'    
+    }
+    Start-Sleep -Seconds 1
+}
+
+try 
+{
+    # Set registry setting for AVD Media Optimization
+    Set-RegistrySetting -Name 'IsWVDEnvironment' -Path 'HKLM:\SOFTWARE\Microsoft\Teams' -PropertyType 'Dword' -Value 1
+
+    # Install Visual C++
+    $URL = 'https://aka.ms/vs/16/release/vc_redist.x64.exe'
+    $File = 'C:\temp\vc_redist.x64.exe'
+    Invoke-WebRequest -Uri $URL -OutFile $File
+    Start-Process -FilePath $File -Args "/install /quiet /norestart /log vcdist.log" -Wait
+    Write-Log -Type 'INFO' -Message 'Installed Visual C++'
+
+    # Install Teams WebSocket Service
+    $URL = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4AQBt'
+    $File = 'C:\temp\webSocketSvc.msi'
+    Invoke-WebRequest -Uri $URL -OutFile $File
+    Start-Process -FilePath msiexec.exe -Args "/i $File /quiet /qn /norestart /passive /log webSocket.log" -Wait
+    Write-Log -Type 'INFO' -Message 'Installed the Teams WebSocket service'
+
+    # Install Teams
+    $URL = 'https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true'
+    $File = 'C:\temp\teams.msi'
+    Invoke-WebRequest -Uri $URL -OutFile $File
+    Start-Process -FilePath msiexec.exe -Args "/i $File /quiet /qn /norestart /passive /log teams.log ALLUSER=1 ALLUSERS=1" -Wait
+    Write-Log -Type 'INFO' -Message 'Installed Teams'
+}
+catch 
+{
+    Write-Log -Message $_ -Type 'ERROR'
+    throw
 }

55b1daae8d8954fcc07d5939f8aad859473b1450
Jason Masten <jamasten@microsoft.com>
2023-04-04 17:30:04 -0400

    Added uninstall option for removing old versions

--- solutions/imageBuilder/scripts/fslogix.ps1
+++ solutions/imageBuilder/scripts/fslogix.ps1
@@ -4,28 +4,45 @@
 $ErrorActionPreference = 'Stop'
 
 try 
-{    
-    # Download the latest version of FSLogix
-    $LandingPage = Invoke-WebRequest -Uri 'https://aka.ms/fslogix-latest'
-    $DownloadButtonUrlSuffix = $LandingPage.Links | Where-Object {$_.href -like "confirmation.aspx?id=*"} | Select-Object -ExpandProperty 'href'
-    $DownloadButtonUrl = 'https://www.microsoft.com/en-us/download/' + $DownloadButtonUrlSuffix
-    $DownloadPage = Invoke-WebRequest -Uri $DownloadButtonUrl
-    $URL = $DownloadPage.Links | Where-Object {$_.href -like "https://download.microsoft.com/download/*"} | Select-Object -ExpandProperty 'href' -First 1
+{
     $File = 'C:\temp\fslogix.zip'
-    Invoke-WebRequest -Uri $URL -OutFile $File
-    Write-Host 'Downloaded latest version of FSLogix'
+    if(Test-Path -Path $File)
+    {
+        Write-Host 'FSLogix installer exists'
+    }
+    else
+    {
+        # Download the latest version of FSLogix
+        $LandingPage = Invoke-WebRequest -Uri 'https://aka.ms/fslogix-latest'
+        $DownloadButtonUrlSuffix = $LandingPage.Links | Where-Object {$_.href -like "confirmation.aspx?id=*"} | Select-Object -ExpandProperty 'href'
+        $DownloadButtonUrl = 'https://www.microsoft.com/en-us/download/' + $DownloadButtonUrlSuffix
+        $DownloadPage = Invoke-WebRequest -Uri $DownloadButtonUrl
+        $URL = $DownloadPage.Links | Where-Object {$_.href -like "https://download.microsoft.com/download/*"} | Select-Object -ExpandProperty 'href' -First 1
+        $File = 'C:\temp\fslogix.zip'
+        Invoke-WebRequest -Uri $URL -OutFile $File
+        Write-Host 'Downloaded latest version of FSLogix'
 
-    # Unblock the ZIP file containing FSLogix
-    Unblock-File -Path $File
-    Write-Host 'Unblocked the ZIP file containing FSLogix'
+        # Unblock the ZIP file containing FSLogix
+        Unblock-File -Path $File
+        Write-Host 'Unblocked the ZIP file containing FSLogix'
 
-    # Expand the ZIP file containing FSLogix
-    Expand-Archive -LiteralPath $File -DestinationPath 'C:\temp\fslogix' -Force
-    Write-Host 'Expanded the ZIP file containing FSLogix'
+        # Expand the ZIP file containing FSLogix
+        Expand-Archive -LiteralPath $File -DestinationPath 'C:\temp\fslogix' -Force
+        Write-Host 'Expanded the ZIP file containing FSLogix'
+    }
 
-    # Install FSLogix silently
-    Start-Process -FilePath 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe' -ArgumentList "/install /quiet" -Wait -PassThru
-    Write-Host 'Installed FSLogix'
+    if(Get-Package -Name 'Microsoft FSLogix Apps')
+    {
+        # Uninstall FSLogix silently
+        Start-Process -FilePath 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe' -ArgumentList "/uninstall /quiet /norestart" -Wait -PassThru
+        Write-Host 'Installed FSLogix'
+    }
+    else 
+    {
+        # Install FSLogix silently
+        Start-Process -FilePath 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe' -ArgumentList "/install /quiet /norestart" -Wait -PassThru
+        Write-Host 'Installed FSLogix'
+    }
 }
 catch 
 {

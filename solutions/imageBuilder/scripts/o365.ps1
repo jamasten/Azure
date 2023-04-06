2df4cf361e5778a0974acc1ac909b83073fb9544
Jason Masten <jamasten@microsoft.com>
2023-04-06 12:08:37 -0400

    Changed method for registry updates

--- solutions/imageBuilder/scripts/o365.ps1
+++ solutions/imageBuilder/scripts/o365.ps1
@@ -3,52 +3,6 @@
 
 $ErrorActionPreference = 'Stop'
 
-function Set-RegistrySetting 
-{
-    Param(
-        [parameter(Mandatory=$false)]
-        [String]$Name,
-
-        [parameter(Mandatory=$false)]
-        [String]$Path,
-
-        [parameter(Mandatory=$false)]
-        [String]$PropertyType,
-
-        [parameter(Mandatory=$false)]
-        [String]$Value
-    )
-
-    # Create registry key(s) if necessary
-    if(!(Test-Path -Path $Path))
-    {
-        New-Item -Path $Path -Force
-    }
-
-    # Checks for existing registry setting
-    $Value = Get-ItemProperty -Path $Path -Name $Name -ErrorAction 'SilentlyContinue'
-    $LogOutputValue = 'Path: ' + $Path + ', Name: ' + $Name + ', PropertyType: ' + $PropertyType + ', Value: ' + $Value
-    
-    # Creates the registry setting when it does not exist
-    if(!$Value)
-    {
-        New-ItemProperty -Path $Path -Name $Name -PropertyType $PropertyType -Value $Value -Force
-        Write-Host "Added registry setting: $LogOutputValue"
-    }
-    # Updates the registry setting when it already exists
-    elseif($Value.$($Name) -ne $Value)
-    {
-        Set-ItemProperty -Path $Path -Name $Name -Value $Value -Force
-        Write-Host "Updated registry setting: $LogOutputValue"
-    }
-    # Writes log output when registry setting has the correct value
-    else 
-    {
-        Write-Host "Registry setting exists with correct value: $LogOutputValue"
-    }
-    Start-Sleep -Seconds 1
-}
-
 $OfficeConfiguration = @'
     <Configuration>
         <Add OfficeClientEdition="64" Channel="Current">
@@ -80,23 +34,22 @@ try
     Start-Process 'reg' -ArgumentList 'load HKU\TempDefault C:\Users\Default\NTUSER.DAT' -Wait -PassThru  | Out-Null
     Write-Host 'Mounted the default user registry hive'
 
-    # Must be executed with the default registry hive mounted.
-    Set-RegistrySetting -Name 'InsiderSlabBehavior' -Path 'HKU\TempDefault\SOFTWARE\Policies\Microsoft\office\16.0\common' -PropertyType 'DWord' -Value 2
-
-    # Set Outlook's Cached Exchange Mode behavior
-    # Must be executed with default registry hive mounted.
-    Set-RegistrySetting -Name 'enable' -Path 'HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode' -PropertyType 'DWord' -Value 1
-    Set-RegistrySetting -Name 'syncwindowsetting' -Path 'HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode' -PropertyType 'DWord' -Value 1
-    Set-RegistrySetting -Name 'CalendarSyncWindowSetting' -Path 'HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode' -PropertyType 'DWord' -Value 1
-    Set-RegistrySetting -Name 'CalendarSyncWindowSettingMonths' -Path 'HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode' -PropertyType 'DWord' -Value 1
+    # Configure default behavior for Office
+    Start-Process 'reg' -ArgumentList 'add "HKU\TempDefault\SOFTWARE\Policies\Microsoft\office\16.0\common" /v InsiderSlabBehavior /t REG_DWORD /d 2 /f' -Wait -PassThru  | Out-Null
+    Start-Process 'reg' -ArgumentList 'add "HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode" /v enable /t REG_DWORD /d 1 /f' -Wait -PassThru  | Out-Null
+    Start-Process 'reg' -ArgumentList 'add "HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode" /v syncwindowsetting /t REG_DWORD /d 1 /f' -Wait -PassThru  | Out-Null
+    Start-Process 'reg' -ArgumentList 'add "HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode" /v CalendarSyncWindowSetting /t REG_DWORD /d 1 /f' -Wait -PassThru  | Out-Null
+    Start-Process 'reg' -ArgumentList 'add "HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode" /v CalendarSyncWindowSettingMonths  /t REG_DWORD /d 1 /f' -Wait -PassThru  | Out-Null
+    Write-Host 'Configured default behavior for Office'
 
     #Unmount the default user registry hive
     Start-Process 'reg' -ArgumentList 'unload HKU\TempDefault' -Wait -PassThru  | Out-Null
     Write-Host 'Unmounted the default user registry hive'
 
     # Set the Office Update UI behavior.
-    Set-RegistrySetting -Name 'hideupdatenotifications' -Path 'HKLM:\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate' -PropertyType 'DWord' -Value 1
-    Set-RegistrySetting -Name 'hideenabledisableupdates' -Path 'HKLM:\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate' -PropertyType 'DWord' -Value 1
+    Start-Process 'reg' -ArgumentList 'add "HKLM\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate" /v hideupdatenotifications /t REG_DWORD /d 1 /f' -Wait -PassThru  | Out-Null
+    Start-Process 'reg' -ArgumentList 'add "HKLM\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate" /v hideenabledisableupdates /t REG_DWORD /d 1 /f' -Wait -PassThru  | Out-Null
+    Write-Host 'Set the default update behavior for Office'
 }
 catch 
 {

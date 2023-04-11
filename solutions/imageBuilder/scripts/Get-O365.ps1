50f981ee356d8ef7783f0c0a04aa88833d8e6848
Jason Masten <jamasten@microsoft.com>
2023-04-10 20:27:45 -0400

    Updated var name

--- solutions/imageBuilder/scripts/Get-O365.ps1
+++ solutions/imageBuilder/scripts/Get-O365.ps1
@@ -1,13 +1,15 @@
 # Download & extract the Office 365 Deployment Toolkit
+
 $ErrorActionPreference = 'Stop'
+
 try 
 {
     $URL = 'https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16130-20218.exe'
-    $File = 'C:\temp\office.exe'
-    Invoke-WebRequest -Uri $URL -OutFile $File
+    $Installer = 'C:\temp\office.exe'
+    Invoke-WebRequest -Uri $URL -OutFile $Installer
     Write-Host 'Downloaded the Office 365 Deployment Toolkit'
 
-    Start-Process -FilePath 'C:\temp\office.exe' -ArgumentList "/extract:C:\temp /quiet /passive /norestart" -Wait -PassThru | Out-Null
+    Start-Process -FilePath $Installer -ArgumentList "/extract:C:\temp /quiet /passive /norestart" -Wait -PassThru | Out-Null
     Write-Host 'Extracted the Office 365 Deployment Toolkit'
 }
 catch 

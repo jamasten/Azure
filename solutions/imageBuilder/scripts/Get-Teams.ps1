7bb12622d80eb3dce01b670e48365793ca27cadd
Jason Masten <jamasten@microsoft.com>
2023-04-10 20:25:58 -0400

    Updated var name

--- solutions/imageBuilder/scripts/Get-Teams.ps1
+++ solutions/imageBuilder/scripts/Get-Teams.ps1
@@ -4,20 +4,20 @@ try
 {
     # Visual C++
     $URL = 'https://aka.ms/vs/16/release/vc_redist.x64.exe'
-    $File = 'C:\temp\vc_redist.x64.exe'
-    Invoke-WebRequest -Uri $URL -OutFile $File
+    $Installer = 'C:\temp\vc_redist.x64.exe'
+    Invoke-WebRequest -Uri $URL -OutFile $Installer
     Write-Host 'Downloaded Visual C++'
 
     # Teams WebSocket Service
     $URL = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4AQBt'
-    $File = 'C:\temp\webSocketSvc.msi'
-    Invoke-WebRequest -Uri $URL -OutFile $File
+    $Installer = 'C:\temp\webSocketSvc.msi'
+    Invoke-WebRequest -Uri $URL -OutFile $Installer
     Write-Host 'Downloaded the Teams WebSocket service'
 
     # Teams
     $URL = 'https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true'
-    $File = 'C:\temp\teams.msi'
-    Invoke-WebRequest -Uri $URL -OutFile $File
+    $Installer = 'C:\temp\teams.msi'
+    Invoke-WebRequest -Uri $URL -OutFile $Installer
     Write-Host 'Downloaded Teams'
 }
 catch 
